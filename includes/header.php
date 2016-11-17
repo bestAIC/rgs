73dfabe8190363bd35880da97cea64696ad314ef
viktor <viktrgulakv@rambler.ru>
2016-11-17 16:53:31 +0300

    upd:шапка,футер

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,8 @@
 			<a href="/" class="header__logo"></a>
 			<a href="/" class="header__logo-ico"></a>
 			<div class="header__top-right">
+				<a href="#" class="header__menu-btn is-tablet-block"></a>
+				<a href="#" class="header__search-btn is-tablet-block"></a>
 				<a href="#" class="btn _gold header__top-btn">ИНТЕРНЕТ-БАНК</a>
 				<div class="header__phones">
 					<div class="header__phone">
@@ -30,22 +32,30 @@
 			</div>
 		</div>
 	</div>
-	<div class="header__bottom hide">
-		<div class="header__menu">
-			<div class="header__menu-item">
-				<a href="#" class="header__menu-link">Вложить и сохранить</a>
-			</div>
-			<div class="header__menu-item">
-				<a href="#" class="header__menu-link">Оплатить и перевести</a>
-			</div>
-			<div class="header__menu-item">
-				<a href="#" class="header__menu-link">Выбрать карту</a>
-			</div>
-			<div class="header__menu-item">
-				<a href="#" class="header__menu-link">Взять кредит</a>
-			</div>
-			<div class="header__menu-item">
-				<a href="#" class="header__menu-link">Инвестировать</a>
+	<div class="header__bottom">
+		<div class="grid__inner">
+			<a href="#" class="header__menu-btn no-tablet-block"></a>
+			<a href="#" class="header__search-btn no-tablet-block"></a>
+			<div class="header__menu">
+				<div class="header__menu-item no-mob-block">
+					<a href="#" class="header__menu-link">Вложить и сохранить</a>
+				</div>
+				<div class="header__menu-item no-mob-block">
+					<a href="#" class="header__menu-link">Оплатить и перевести</a>
+				</div>
+				<div class="header__menu-item no-mob-block">
+					<a href="#" class="header__menu-link">Выбрать карту</a>
+				</div>
+				<div class="header__menu-item no-mob-block">
+					<a href="#" class="header__menu-link">Взять кредит</a>
+				</div>
+				<div class="header__menu-item no-mob-block">
+					<a href="#" class="header__menu-link">Инвестировать</a>
+				</div>
+
+				<div class="header__menu-item is-mob-block">
+					<a href="#" class="header__menu-link">Меню</a>
+				</div>
 			</div>
 		</div>
 	</div>
