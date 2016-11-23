65656908d7cde4369b248c5b5d7cd052524221ac
viktor <viktrgulakv@rambler.ru>
2016-11-23 17:36:06 +0300

    upd:скрыл элементы на главной

--- static/index.php
+++ static/index.php
@@ -3,86 +3,8 @@
 		<?include "includes/top-banner.php"?>
 		<?include "includes/exchange.php"?>
 		<?include "includes/products.php"?>
-		<?include "includes/services.php"?>
+		<?/*include "includes/services.php"*/?>
 		<div class="home__gray">
-			<div class="home__transfer">
-				<div class="grid__inner">
-					<h2>Переводите с карты на карту</h2>
-					<div class="subtitle">Переводы на карту любого российского банка, надежный сервис с выгодными тарифами</div>
-					<form action="">
-						<div class="home__transfer-cards" data-animation>
-							<div class="home__transfer-card _1 card">
-								<div class="card__inner">
-									<div class="card__title">1. Отправитель</div>
-									<div class="card__fields">
-										<div class="card__fields-col _1">
-											<div class="card__block">
-												<div class="card__block-title">Номер карты отправителя<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="text" placeholder="0000 0000 0000 0000" data-card-mask>
-												</div>
-											</div>
-											<div class="card__block">
-												<div class="card__block-title">Владелец карты<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="text" placeholder="NIKOLAY Ivanov">
-												</div>
-											</div>
-										</div>
-										<div class="card__fields-col _2">
-											<div class="card__block">
-												<div class="card__block-title">cvc2 / cvv2<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="password" placeholder="&#8226;&#8226;&#8226;" data-cvv-mask>
-												</div>
-											</div>
-											<div class="card__block">
-												<div class="card__block-title">Срок действия<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="text" data-date-mask placeholder="ММ / ГГ">
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="home__transfer-card _2 _light card">
-								<div class="card__inner">
-									<div class="card__title">2. Получатель</div>
-									<div class="card__fields">
-										<div class="card__fields-col _1">
-											<div class="card__block">
-												<div class="card__block-title">Номер карты получателя<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="text" placeholder="0000 0000 0000 0000" data-card-mask>
-												</div>
-											</div>
-											<div class="card__block">
-												<div class="card__block-title">Владелец карты<a href="#" class="help-ico">?</a></div>
-												<div class="card__block-field">
-													<input type="text" placeholder="Ivan PETROV">
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="home__transfer-block">
-							<div class="home__transfer-block-in">
-								<div class="home__transfer-sum">
-									<input type="text" placeholder="Сумма перевода">
-								</div>
-								<div class="home__transfer-commission">
-									<div class="home__transfer-commission-title">Комиссия за перевод</div>
-									<div class="home__transfer-commission-val">0 руб.</div>
-								</div>
-								<button type="submit" class="btn _red home__transfer-btn">Отправить деньги</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
 			<?include "includes/info-center.php"?>
 			<?include "includes/news-gall.php"?>
 		</div>
