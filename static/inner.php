0219dc6b28fb07c9fb1a962414cb9ac675278142
viktor <viktrgulakv@rambler.ru>
2016-11-24 18:47:23 +0300

    upd:пенсионная программа

--- static/inner.php
+++ static/inner.php
@@ -18,6 +18,90 @@
 				</div>
 			</div>
 		</div>
+		<div class="inner__info">
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="numerals">
+						<div class="numerals__block">
+							<div class="numerals__item _big">
+								<div class="numerals__item-val">5,5%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									годовых на сумму, превышающую<br/>
+									20 000 рублей
+								</div>
+							</div>
+						</div>
+						<div class="numerals__block">
+							<div class="numerals__item _small">
+								<div class="numerals__item-val">3,5%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									годовых на сумму,<br/>
+									превышающую,<br/>
+									5 000 рублей,<br/>
+									и не более,<br/>
+									20 000 рублей
+								</div>
+							</div>
+							<div class="numerals__item _small">
+								<div class="numerals__item-val">0,1%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									годовых на сумму,<br/>
+									менее 5 000 рублей
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="files">
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+								<div class="file__props">PDF, 1.16MB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
+								<div class="file__props">PDF, 768.81KB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Анкета заявление на получение банковской карты для тарифа «Карта Пенсионная»</div>
+								<div class="file__props">DOC, 89KB</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						Переводите пенсию на карту РОСГОССТРАХ БАНКА<br />
+						и получайте преимущества в рамках Пенсионной программы.
+					</div>
+					<ul class="b-list">
+						<li>Бесплатная выдача карты и обслуживание банковского счета</li>
+						<li>
+							Начисление процентов на среднемесячный остаток средств
+							по карте
+						</li>
+						<li><a href="#">Кэшбэк (возврат средств)</a> - 3% от суммы покупок по карте в аптеках</li>
+						<li>Бесплатная выдача наличных средств в банкоматах Сбербанка, Московского Кредитного Банка и РОСГОССТРАХ БАНКА</li>
+						<li>Повышенная ставка по вкладу <a href="#">«Пенсионный доход»</a></li>
+						<li>
+							<a href="#">Специальное предложение по вкладу «Пенсионный доход»</a>
+							для держателей «Карты Пенсионная» (до 17.12.2016г.)
+						</li>
+						<li>Бесплатный <a href="#">Интернет-Банк</a> и SMS-информирование</li>
+						<li><a href="#">Скидки и привилегии в компаниях-партнерах</a> Банка</li>
+					</ul>
+					<div class="inner__info-benefits-btn">
+						<a href="#" class="btn _red">ПРИСОЕДИНИТЬСЯ К ПЕНСИОННОЙ ПРОГРАММЕ</a>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="b-blocks _gray">
 			<?include "includes/subscription.php"?>
 			<?include "includes/offices.php"?>
