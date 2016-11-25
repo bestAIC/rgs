228a944180ab415011f0f71df5e03293f1704565
viktor <viktrgulakv@rambler.ru>
2016-11-25 20:24:34 +0300

    upd:документы

--- static/inner.php
+++ static/inner.php
@@ -54,27 +54,6 @@
 							</div>
 						</div>
 					</div>
-					<?/*файлы десктоп*/?>
-					<div class="files is-desktop-block">
-						<a href="#" class="file">
-							<div class="file__inner">
-								<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
-								<div class="file__props">PDF, 1.16MB</div>
-							</div>
-						</a>
-						<a href="#" class="file">
-							<div class="file__inner">
-								<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
-								<div class="file__props">PDF, 768.81KB</div>
-							</div>
-						</a>
-						<a href="#" class="file">
-							<div class="file__inner">
-								<div class="file__name">Анкета заявление на получение банковской карты для тарифа «Карта Пенсионная»</div>
-								<div class="file__props">DOC, 89KB</div>
-							</div>
-						</a>
-					</div>
 				</div>
 				<div class="inner__info-benefits">
 					<div class="inner__info-benefits-title">
@@ -100,21 +79,29 @@
 					<div class="inner__info-benefits-btn">
 						<a href="#" class="btn _red">ПРИСОЕДИНИТЬСЯ К ПЕНСИОННОЙ ПРОГРАММЕ</a>
 					</div>
-					<?/*файлы планшет и мобильная*/?>
-					<div class="files no-desktop-block">
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "includes/subscription.php"?>
+			<?include "includes/offices.php"?>
+			<div class="b-block questions animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Полезные документы</h2>
+					<div class="files">
 						<a href="#" class="file">
 							<div class="file__inner">
 								<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
 								<div class="file__props">PDF, 1.16MB</div>
 							</div>
-						</a>
-						<a href="#" class="file">
+						</a><!---
+						--><a href="#" class="file">
 							<div class="file__inner">
 								<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
 								<div class="file__props">PDF, 768.81KB</div>
 							</div>
-						</a>
-						<a href="#" class="file">
+						</a><!---
+						--><a href="#" class="file">
 							<div class="file__inner">
 								<div class="file__name">Анкета заявление на получение банковской карты для тарифа «Карта Пенсионная»</div>
 								<div class="file__props">DOC, 89KB</div>
@@ -123,10 +110,6 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="b-blocks _gray">
-			<?include "includes/subscription.php"?>
-			<?include "includes/offices.php"?>
 			<?include "includes/questions.php"?>
 			<?include "includes/news-gall.php"?>
 		</div>
