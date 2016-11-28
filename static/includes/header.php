a3d4cd909059d05033d9ee37f619d4b040254bb8
viktor <viktrgulakv@rambler.ru>
2016-11-28 11:24:26 +0300

    upd:меню

--- static/includes/header.php
+++ static/includes/header.php
@@ -63,32 +63,207 @@
 		<div class="header__menu-blocks">
 			<div class="grid__inner">
 				<?include "menu/burger/b-menu.php"?>
-				<div class="menu__block _1 hide" data-menu-content="1">
-					меню1<br/>
-					меню1<br/>
-					меню1<br/>
-					меню1<br/>
-					меню1<br/>
+				<div class="menu__block hide" data-menu-content="1">
+					<div class="menu__block-inner">
+						<div class="menu-banner">
+							<div class="menu-banner__img" style="background-image: url('theme/images/menu-banner.jpg');"></div>
+							<div class="menu-banner__inner">
+								<div class="menu-banner__title">Пенсионная программа</div>
+								<div class="menu-banner__text">
+									Получайте скидки в аптеках и повышенную
+									ставку по вкладу «Пенсионный доход»
+								</div>
+								<a href="#" class="btn _red menu-banner__btn">Оформить заявку</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-1-1">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Вклады</a>
+							</div>
+							<div class="b-menu__submenu">
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Госстраховский»</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Накопительный»</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Универсальный»</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Инвестиционный»</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Пенсионный доход»</a>
+								</div>
+								<a href="#" class="btn menu__block-btn _red">подобрать вклад</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-1-2">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Пенсионная программа</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Текущие счета</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Сейфовые ячейки</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-1-3">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Другие услуги</a>
+							</div>
+							<div class="b-menu__submenu">
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">Выплаты вкладчикам других банков</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">Компенсационные выплаты</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="menu__block _1 hide" data-menu-content="2">
-					меню2<br/>
-					меню2<br/>
-					меню2<br/>
-					меню2<br/>
-					меню2<br/>
+				<div class="menu__block hide" data-menu-content="2">
+					<div class="menu__block-inner">
+						<div class="menu-banner">
+							<div class="menu-banner__img" style="background-image: url('theme/images/menu-banner.jpg');"></div>
+							<div class="menu-banner__inner">
+								<div class="menu-banner__title">Переводы по номеру карты</div>
+								<div class="menu-banner__text">
+									Моментальные переводы между картами,
+									выпущенными российскими банками
+								</div>
+								<a href="#" class="btn _red menu-banner__btn">УЗНАТЬ ПОДРОБНЕЕ</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-2-1">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Оплата услуг</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">С карты на карту</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">
+									Валютно-обменные<br />
+									операции по льготному курсу
+								</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-2-2">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Денежные переводы</a>
+							</div>
+							<div class="b-menu__submenu">
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">Мгновенные переводы РОСГОССТРАХ БАНКА</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">Western Union</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">MoneyGram</a>
+								</div>
+								<div class="b-menu__submenu-item">
+									<a href="#" class="b-menu__submenu-link">«Золотая корона»</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="menu__block _1 hide" data-menu-content="3">
-					меню3
+					<div class="menu__block-inner">
+						<div class="menu-banner">
+							<div class="menu-banner__img" style="background-image: url('theme/images/menu-banner.jpg');"></div>
+							<div class="menu-banner__inner">
+								<div class="menu-banner__title">Пенсионная карта</div>
+								<div class="menu-banner__text">
+									Откройте пенсионную карту и получайте
+									возврат 3% от покупок в аптеках
+								</div>
+								<a href="#" class="btn _red menu-banner__btn">УЗНАТЬ ПОДРОБНЕЕ</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-3-1">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Отличная карта</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Пенсионная карта</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Зарплатная карта</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Visa и MasterCard</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Расчетная карта</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Карта Клиента</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Премиальная карта</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-3-2">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">
+									Кредитные карты для сотрудников
+									группы Росгосстрах
+								</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">
+									Скидки и специальные
+									предложения
+								</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Пополнение счета</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="menu__block _1 hide" data-menu-content="4">
-					меню4<br/>
+					<div class="menu__block-inner">
+						<div class="menu__block-col _col-4-1">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Кредит наличными</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Ипотека</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-4-2">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Под залог недвижимости</a>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="menu__block _1 hide" data-menu-content="5">
-					меню5<br/>
-					меню5<br/>
-					меню5<br/>
-					меню5<br/>
-					меню5<br/>
+					<div class="menu__block-inner">
+						<div class="menu__block-col _col-5-1">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Индивидуальный инвестиционный счет</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Инвестиционное страхование жизни</a>
+							</div>
+						</div>
+						<div class="menu__block-col _col-5-2">
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">ПИФы</a>
+							</div>
+							<div class="b-menu__item">
+								<a href="#" class="b-menu__item-link">Депозитарий</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
