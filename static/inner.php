0ef1786a224742ae006e2bbc326096578fcfe0e3
viktor <viktrgulakv@rambler.ru>
2016-11-28 14:11:43 +0300

    fix:правки

--- static/inner.php
+++ static/inner.php
@@ -110,6 +110,31 @@
 					</div>
 				</div>
 			</div>
+			<div class="b-block questions animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Полезные документы в 2 столбца</h2>
+					<div class="files _two-cols">
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+								<div class="file__props">PDF, 1.16MB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
+								<div class="file__props">PDF, 768.81KB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Анкета заявление на получение банковской карты для тарифа «Карта Пенсионная»</div>
+								<div class="file__props">DOC, 89KB</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
 			<?include "includes/questions.php"?>
 			<?include "includes/news-gall.php"?>
 		</div>
