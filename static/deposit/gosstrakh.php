521c47efb9a7f5fc59cf7dd1c4b254d0e5353d42
viktor <viktrgulakv@rambler.ru>
2016-11-30 16:37:48 +0300

    upd:вклад

--- static/deposit/gosstrakh.php
+++ static/deposit/gosstrakh.php
@@ -69,8 +69,149 @@
 					</div>
 				</div>
 			</div>
-			<div class="b-block animation__top" data-animation>
-				<div class="grid__inner">Классический вклад для получения высокого дохода при фиксированных сумме и сроке вклада</div>
+			<div class="b-block animation__top deposit-info" data-animation>
+				<div class="grid__inner">
+					<div class="deposit-info__conditions">
+						<h3>Условия доходности по вкладу</h3>
+						<table class="deposit-table">
+							<tr>
+								<th>
+									Срок вклада<br/>
+									(дней)
+								</th>
+								<th></th>
+								<th>91-180</th>
+								<th>181-366</th>
+								<th>367-732</th>
+							</tr>
+							<tr class="deposit-table__titles">
+								<td class="center">ВАЛЮТА ВКЛАДА</td>
+								<td>СУММА ВКЛАДА</td>
+								<td colspan="3" class="center">ГОДОВАЯ ПРОЦЕНТНАЯ СТАВКА, %</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">₽</td>
+								<td>от 5 000</td>
+								<td class="center bold">7,70</td>
+								<td class="center bold">8,00</td>
+								<td class="center bold">7,00</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">$</td>
+								<td>от 100</td>
+								<td class="center bold">0,75</td>
+								<td class="center bold">1,80</td>
+								<td class="center bold">2,00</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">€</td>
+								<td>от 100</td>
+								<td class="center bold">0,30</td>
+								<td class="center bold">0,60</td>
+								<td class="center bold">0,90</td>
+							</tr>
+						</table>
+						<h3>Условия доходности по вкладу</h3>
+						<table class="deposit-table">
+							<tr>
+								<th>
+									Срок вклада<br/>
+									(дней)
+								</th>
+								<th>181-366</th>
+								<th>367-732</th>
+							</tr>
+							<tr class="deposit-table__titles">
+								<td class="center">ВАЛЮТА ВКЛАДА</td>
+								<td colspan="2" class="center">ГОДОВАЯ ПРОЦЕНТНАЯ СТАВКА, %</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">₽</td>
+								<td class="center bold">7,70</td>
+								<td class="center bold">8,00</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">$</td>
+								<td class="center bold">0,75</td>
+								<td class="center bold">1,80</td>
+							</tr>
+							<tr class="deposit-table__row">
+								<td class="center">€</td>
+								<td class="center bold">0,30</td>
+								<td class="center bold">0,90</td>
+							</tr>
+						</table>
+						<h3>Условия вклада</h3>
+						<div class="deposit-conditions">
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Тип вклада</div>
+								<div class="deposit-conditions__item-block">срочный</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Валюта вклада</div>
+								<div class="deposit-conditions__item-block">рубли, доллары США, евро</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Срок размещения</div>
+								<div class="deposit-conditions__item-block">
+									минимальный - 181 день,
+									максимальный - 732 дня
+								</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Минимальная сумма вклада</div>
+								<div class="deposit-conditions__item-block">
+									1 000 рублей / 50 долларов США
+									/ 50 евро
+								</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Пополнение вклада</div>
+								<div class="deposit-conditions__item-block">
+									предусмотрено, не позднее, чем
+									за 3 месяца до окончания срока вклада
+								</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">
+									Минимальная сумма
+									дополнительного взноса на вклад
+								</div>
+								<div class="deposit-conditions__item-block">не ограничена</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Частичное снятие вклада</div>
+								<div class="deposit-conditions__item-block">не предусмотрено</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Выплата процентов</div>
+								<div class="deposit-conditions__item-block">
+									ежемесячно путем перечисления
+									на текущий счет или
+									на СКС Вкладчика, открытый в Банке
+								</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">Пролонгация</div>
+								<div class="deposit-conditions__item-block">
+									автоматическая пролонгация
+									по окончании срока вклада
+									на тот же срок с учетом ставки
+									по вкладу на момент пролонгации
+								</div>
+							</div>
+							<div class="deposit-conditions__item">
+								<div class="deposit-conditions__item-block">
+									Процентная ставка
+									при досрочном расторжении
+								</div>
+								<div class="deposit-conditions__item-block">
+									0,1%
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="b-blocks _gray">
