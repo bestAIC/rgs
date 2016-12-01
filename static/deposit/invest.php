84fbf8c19aaaed9dc22c335bcd5f38f4db1f9c77
viktor <viktrgulakv@rambler.ru>
2016-12-01 16:02:08 +0300

    add:вклады

---
+++ static/deposit/invest.php
@@ -0,0 +1,410 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner" style="background-image: url('/static/theme/images/inner/banner.jpg');">
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Вклад «Инвестиционный»</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Частным клиентам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Вложить и сохранить</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Вклад «Инвестиционный»</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="numerals">
+						<div class="numerals__block">
+							<div class="numerals__item _big">
+								<div class="numerals__item-val">9,50%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									годовая процентная ставка на вклад в рублях
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						Высокодоходный вклад с возможностью получить страховую защиту жизни и здоровья или дополнительный доход от вложения средств в ПИФы
+					</div>
+					<p>
+						Позволяет получить более высокую доходность сбережений за счет комбинации гарантированного дохода на сумму вклада и возможности получения дополнительного инвестиционного дохода от вложения средств в паи ПИФов
+						под управлением ООО «Управление Сбережениями» или получить страховую защиту жизни и здоровья при заключении договора страхования жизни
+						с компанией ООО «Страховая компания «Росгосстрах-Жизнь»».
+					</p>
+					<div class="inner__info-benefits-btn">
+						<a href="#" class="btn _red">Открыть вклад</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks">
+			<?include "../includes/calc.php"?>
+			<div class="b-block animation__top deposit-info" data-animation>
+				<div class="grid__inner">
+					<div class="deposit-info__left">
+						<div class="deposit-info__block">
+							<h3>Условия доходности по вкладу</h3>
+							<div class="deposit-table__wrap">
+								<table class="deposit-table">
+									<tr>
+										<th class="is-mob-block" colspan="2">
+											Срок вклада<br/>
+											(дней)
+										</th>
+										<th class="no-mob-block">
+											Срок вклада<br/>
+											(дней)
+										</th>
+										<th  class="no-mob-block"></th>
+										<th>91-180</th>
+										<th>181-366</th>
+										<th>367-732</th>
+									</tr>
+									<tr class="deposit-table__titles">
+										<td class="is-mob-block" colspan="2">ВАЛЮТА ВКЛАДА</td>
+										<td class="no-mob-block center">ВАЛЮТА ВКЛАДА</td>
+										<td class="no-mob-block">СУММА ВКЛАДА</td>
+										<td colspan="3" class="center">ГОДОВАЯ ПРОЦЕНТНАЯ СТАВКА, %</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">₽</td>
+										<td >от 5 000</td>
+										<td class="center bold">7,70</td>
+										<td class="center bold">8,00</td>
+										<td class="center bold">7,00</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">$</td>
+										<td>от 100</td>
+										<td class="center bold">0,75</td>
+										<td class="center bold">1,80</td>
+										<td class="center bold">2,00</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">€</td>
+										<td>от 100</td>
+										<td class="center bold">0,30</td>
+										<td class="center bold">0,60</td>
+										<td class="center bold">0,90</td>
+									</tr>
+								</table>
+							</div>
+
+						</div>
+						<div class="deposit-info__block">
+							<h3>Условия доходности по вкладу</h3>
+							<div class="deposit-table__wrap">
+								<table class="deposit-table">
+									<tr>
+										<th>
+											Срок вклада<br/>
+											(дней)
+										</th>
+										<th>181-366</th>
+										<th>367-732</th>
+									</tr>
+									<tr class="deposit-table__titles">
+										<td class="center">ВАЛЮТА ВКЛАДА</td>
+										<td colspan="2" class="center">ГОДОВАЯ ПРОЦЕНТНАЯ СТАВКА, %</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">₽</td>
+										<td class="center bold">7,70</td>
+										<td class="center bold">8,00</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">$</td>
+										<td class="center bold">0,75</td>
+										<td class="center bold">1,80</td>
+									</tr>
+									<tr class="deposit-table__row">
+										<td class="center">€</td>
+										<td class="center bold">0,30</td>
+										<td class="center bold">0,90</td>
+									</tr>
+								</table>
+							</div>
+
+						</div>
+						<div class="deposit-info__block">
+							<h3>Условия вклада</h3>
+							<div class="deposit-conditions">
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Тип вклада</div>
+									<div class="deposit-conditions__item-block">срочный</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Валюта вклада</div>
+									<div class="deposit-conditions__item-block">рубли, доллары США, евро</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Срок размещения</div>
+									<div class="deposit-conditions__item-block">
+										минимальный - 181 день,
+										максимальный - 732 дня
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Минимальная сумма вклада</div>
+									<div class="deposit-conditions__item-block">
+										1 000 рублей / 50 долларов США
+										/ 50 евро
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Пополнение вклада</div>
+									<div class="deposit-conditions__item-block">
+										предусмотрено, не позднее, чем
+										за 3 месяца до окончания срока вклада
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">
+										Минимальная сумма
+										дополнительного взноса на вклад
+									</div>
+									<div class="deposit-conditions__item-block">не ограничена</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Частичное снятие вклада</div>
+									<div class="deposit-conditions__item-block">не предусмотрено</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Выплата процентов</div>
+									<div class="deposit-conditions__item-block">
+										ежемесячно путем перечисления
+										на текущий счет или
+										на СКС Вкладчика, открытый в Банке
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Пролонгация</div>
+									<div class="deposit-conditions__item-block">
+										автоматическая пролонгация
+										по окончании срока вклада
+										на тот же срок с учетом ставки
+										по вкладу на момент пролонгации
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">
+										Процентная ставка
+										при досрочном расторжении
+									</div>
+									<div class="deposit-conditions__item-block">
+										0,1%
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__right">
+						<div class="deposit-open deposit-info__block">
+							<h3>Как оформить вклад?</h3>
+							<div class="deposit-open__methods">
+								<div class="deposit-open__method">
+									<div class="deposit-open__method-title">В отделении</div>
+									<div class="deposit-open__method-content">
+										<div class="deposit-open__steps">
+											<div class="deposit-open__step">
+												<div class="deposit-open__step-num">01</div>
+												Обратитесь в любое удобное для Вас отделение РОСГОССТРАХ БАНКА
+												в вашем регионе
+											</div><!--
+											--><div class="deposit-open__step">
+												<div class="deposit-open__step-num">02</div>
+												Предъявите паспорт или
+												<a href="#">иной документ, удостоверяющий личность</a>
+											</div><!--
+											--><div class="deposit-open__step">
+												<div class="deposit-open__step-num">03</div>
+												Заполните
+												<a href="#">Вопросник для физических лиц</a>
+											</div><!--
+											--><div class="deposit-open__step">
+												<div class="deposit-open__step-num">04</div>
+												Подпишите договор
+												о вкладе с Банком
+											</div><!--
+											--><div class="deposit-open__step">
+												<div class="deposit-open__step-num">05</div>
+												Внесите на свой счет сумму вклада
+											</div><!--
+											--><div class="deposit-open__step">
+												<div class="deposit-open__step-num">06</div>
+												Оформите при необходимости
+												<a href="#">доверенность
+												и завещательное распоряжение
+												по вкладу</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="deposit-open__method">
+									<div class="deposit-open__method-title"><a href="#">В Интернет-банке</a></div>
+									<div class="deposit-open__method-content">
+										Вы можете открыть вклад в системе <a href="#">"Интернет-банк"</a>, если&nbsp;у Вас оформлен доступ в систему
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "../includes/subscription.php"?>
+			<?include "../includes/offices.php"?>
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<div class="deposit-doc" data-nav-block>
+						<a href="javascript:void(0);" data-nav-btn class="deposit-doc__title">Памятка вкладчика</a>
+						<div class="deposit-doc__content hide" data-nav-content>
+							<h3>Государственное страхование вкладов</h3>
+							<p>
+								РОСГОССТРАХ БАНК входит в государственную систему страхования вкладов (№279 в реестре банков
+								— участников системы обязательного страхования вкладов).
+								Банк соответствует всем требованиям Банка России, предъявляемым к участникам системы.
+								Сохранность Ваших средств гарантируется государством.
+							</p>
+							<p><strong>Гарантии государства по банковским вкладам составляют 1 400 000 рублей.</strong></p>
+							<div class="questions__items" data-questions>
+								<?
+									$answer = 'Система обязательного страхования вкладов — это специальная программа, созданная государством, которая 
+									работает на основании Федерального закона от 23.12.2003 № 177-ФЗ «О страховании вкладов физических лиц 
+									в банках Российской Федерации». Данная программа создана с целью защиты сбережений населения во вкладах 
+									и счетах в российских банках на территории Российской Федерации.';
+									$questions = [
+										"Что такое система обязательного страхования вкладов?",
+										"Как работает система обязательного страхования вкладов?",
+										"В каком размере выплачивается возмещение по вкладам?",
+										"Какие средства являются застрахованными?",
+										"Как происходит выплата вклада?",
+										"В какие сроки может быть выплачено возмещение по вкладу?",
+										"Как я пойму, застрахован ли вклад в моем Банке?",
+										"А если мой вклад выше 1 400 000 рублей?"
+									];
+									for ($i=0;$i<count($questions);$i++){
+										?>
+										<div class="questions__item" data-questions-item>
+											<div class="questions__item-inner">
+												<a href="javascript:void(0);" class="questions__item-title" data-questions-item-title><?=$questions[$i]?></a>
+												<div class="questions__item-answer" data-questions-item-answer>
+													<?=$answer?>
+												</div>
+											</div>
+										</div>
+									<?}?>
+							</div>
+							<h3>Оформление доверенности</h3>
+							<p>
+								В РОСГОССТРАХ БАНКЕ любой вкладчик может оформить доверенность на распоряжение вкладом. Доверенность должна быть
+								удостоверена нотариально или же должностными лицами, уполномоченными на совершение нотариальных действий.
+								Доверенность на получение сумм со вклада в рублях и в иностранной валюте может быть оформлена вкладчиком
+								в подразделении Банка, где хранится вклад. Оформление доверенности осуществляется в Банке бесплатно.
+							</p>
+							<div class="questions__items" data-questions>
+								<?
+									$questions = [
+										"К нотариально удостоверенным доверенностям приравниваются:"
+									];
+									for ($i=0;$i<count($questions);$i++){
+										?>
+										<div class="questions__item" data-questions-item>
+											<div class="questions__item-inner">
+												<a href="javascript:void(0);" class="questions__item-title" data-questions-item-title><?=$questions[$i]?></a>
+												<div class="questions__item-answer" data-questions-item-answer>
+													<ul>
+														<li>
+															доверенности военнослужащих и других лиц, находящихся на излечении в госпиталях, санаториях и других
+															военно-лечебных учреждениях, удостоверенные начальником такого учреждения, его заместителем
+															по медицинской части, старшим или дежурным врачом;
+														</li>
+														<li>
+															доверенности военнослужащих, а в пунктах дислокации воинских частей, соединений, учреждений
+															и военно-учебных заведений, где нет нотариальных контор и других органов, совершающих нотариальные
+															действия, также доверенности рабочих и служащих, членов их семей и членов семей военнослужащих,
+															удостоверенные командиром (начальником) этих части, соединения, учреждения или заведения;
+														</li>
+														<li>
+															доверенности лиц, находящихся в местах лишения свободы, удостоверенные начальником соответствующего
+															места лишения свободы;
+														</li>
+														<li>
+															доверенности совершеннолетних дееспособных граждан, находящихся в учреждениях социальной защиты
+															населения, удостоверенные администрацией этого учреждения или руководителем (его заместителем)
+															соответствующего органа социальной защиты населения.
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									<?}?>
+							</div>
+							<h3>Налогообложение</h3>
+							<p>
+								Доходы в виде процентов, полученные физическим лицом по вкладу в банке, подлежат налогообложению, если процентная
+								ставка по вкладу превышает:
+							</p>
+							<ul>
+								<li>ставку рефинансирования Центрального банка Российской Федерации, увеличенную на десять процентных
+									пунктов, по вкладам в валюте Российской Федерации;</li>
+								<li>9% годовых по вкладам в иностранной валюте.</li>
+							</ul>
+							<div class="questions__items" data-questions>
+								<?
+									$answer = 'Система обязательного страхования вкладов — это специальная программа, созданная государством, которая 
+									работает на основании Федерального закона от 23.12.2003 № 177-ФЗ «О страховании вкладов физических лиц 
+									в банках Российской Федерации». Данная программа создана с целью защиты сбережений населения во вкладах 
+									и счетах в российских банках на территории Российской Федерации.';
+									$questions = [
+										"Пример",
+										"Оформление завещательного распоряжения"
+									];
+									for ($i=0;$i<count($questions);$i++){
+										?>
+										<div class="questions__item" data-questions-item>
+											<div class="questions__item-inner">
+												<a href="javascript:void(0);" class="questions__item-title" data-questions-item-title><?=$questions[$i]?></a>
+												<div class="questions__item-answer" data-questions-item-answer>
+													<?=$answer?>
+												</div>
+											</div>
+										</div>
+									<?}?>
+							</div>
+						</div>
+					</div>
+					<div class="files _two-cols">
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Тарифы по оплате услуг, предоставляемых физическим лицам</div>
+								<div class="file__props">PDF, 450.96KB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Вопросник для физических лиц</div>
+								<div class="file__props">DOC, 70.42KB</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/questions.php"?>
+			<?include "../includes/news-gall.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
