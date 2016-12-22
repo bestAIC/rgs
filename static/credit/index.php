d547166c8c63dd443dda5599a1482efd970f27bc
viktor <viktrgulakv@rambler.ru>
2016-12-22 18:45:24 +0300

    Add:взять кредит

---
+++ static/credit/index.php
@@ -0,0 +1,122 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit transfers">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/inner/banner/credit/index.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Взять кредит</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Частным клиентам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Взять кредит</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="numerals">
+						<div class="numerals__block">
+							<div class="numerals__item _big">
+								<div class="numerals__item-val">11,75%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									минимальная процентная ставка по ипотеке
+								</div>
+							</div>
+						</div>
+						<div class="numerals__block">
+							<div class="numerals__item _small">
+								<div class="numerals__item-val">21%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									минимальная
+									процентная ставка
+									кредита под залог
+									недвижимости
+								</div>
+							</div>
+							<div class="numerals__item _small">
+								<div class="numerals__item-val">9%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									минимальная процентная
+									ставка кредита наличными
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						Иногда Вы думаете, что есть вещи, которые Вам недоступны. Возможно, это так, когда дело касается нематериальных потребностей. Для всего остального можно взять кредит!
+					</div>
+					<ul class="b-list no-b-indent">
+						<li>Получение кредита в любой из более чем 250 точек продаж
+							в 150 населенных пунктах по всей России
+						</li>
+						<li>Программы кредитования на любые цели, с обеспечением и без</li>
+						<li>Скорость рассмотрения кредитной заявки по ряду продуктов —
+							от 15 минут
+						</li>
+						<li>Льготные условия для лояльных заемщиков</li>
+						<li>Привлекательные условия по ставкам</li>
+						<li>Отсутствие комиссий за выдачу и обслуживание любого потребительского кредита</li>
+						<li>Отсутствие штрафов и моратория за досрочное погашение</li>
+						<li>Множество удобных вариантов <a href="#">погашения кредита</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top credit-nav page-nav" data-animation>
+				<div class="grid__inner">
+					<div class="page-nav__row">
+						<div class="page-nav__row-col">
+							<a href="#" class="page-nav__item _big credit-nav__main">
+								<div class="page-nav__item-title">
+									Кредит наличными
+								</div>
+								<div class="page-nav__item-text">
+									Кредит для надежных клиентов:
+									платя вовремя вы получаете
+									двойную скидку каждые 6 месяцев
+								</div>
+								<div class="credit-nav__main-img"></div>
+							</a>
+						</div>
+						<div class="page-nav__row-col _items">
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-ico _base _services"></div>
+								<div class="page-nav__item-title">
+									Ипотека
+								</div>
+								<div class="page-nav__item-text">
+									Кредитование на покупку квартиры
+								</div>
+							</a>
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-ico _base _currency"></div>
+								<div class="page-nav__item-title">
+									Ипотечное кредитование
+									(под залог недвижимости)
+								</div>
+								<div class="page-nav__item-text">
+									Ипотечный кредит на потребительские цели
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/news-gall.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
