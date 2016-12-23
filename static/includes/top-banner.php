f49c97b45c33ee8973ddaf966777cb23285f0dd5
viktor <viktrgulakv@rambler.ru>
2016-12-23 16:17:42 +0300

    fix:главный баннер

--- static/includes/top-banner.php
+++ static/includes/top-banner.php
@@ -1,7 +1,7 @@
 <div class="swiper-container top-banner" data-banner-gall>
 	<div class="swiper-wrapper">
 		<div class="swiper-slide top-banner__slide">
-			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/main_banner1.png');"></div>
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/rgs_main_banner1.png');"></div>
 			<div class="top-banner__slide-block grid__inner">
 				<div class="top-banner__slide-block-in">
 					<div class="top-banner__slide-title">
@@ -14,7 +14,7 @@
 			</div>
 		</div>
 		<div class="swiper-slide top-banner__slide">
-			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/main_banner2.png');"></div>
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/rgs_main_banner2.png');"></div>
 			<div class="top-banner__slide-block grid__inner">
 				<div class="top-banner__slide-block-in">
 					<div class="top-banner__slide-title">
@@ -26,7 +26,7 @@
 			</div>
 		</div>
 		<div class="swiper-slide top-banner__slide">
-			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/main_banner3.png');"></div>
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/rgs_main_banner3.png');"></div>
 			<div class="top-banner__slide-block grid__inner">
 				<div class="top-banner__slide-block-in">
 					<div class="top-banner__slide-title">
