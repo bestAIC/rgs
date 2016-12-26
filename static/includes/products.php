a9393916796dc92350b59e7f321c556d950d98f5
viktor <viktrgulakv@rambler.ru>
2016-12-26 19:36:47 +0300

    upd:продукты

--- static/includes/products.php
+++ static/includes/products.php
@@ -1,5 +1,5 @@
 <?/*планшет и мобильная версии*/?>
-<div class="products swiper-container no-desktop-block"  data-tablet-slider>
+<?/*div class="products swiper-container no-desktop-block"  data-tablet-slider>
 	<div class="swiper-wrapper">
 		<a href="#" class="product _green swiper-slide" style="background-image: url('/static/theme/images/home/product/1.jpg')">
 			<div class="product__inner">
@@ -45,7 +45,7 @@
 			</div>
 		</a>
 	</div>
-</div>
+</div*/?>
 <?/*десктоп*/?>
 <?/*div class="products is-desktop-block" data-animation>
 	<div class="grid__inner">
@@ -94,13 +94,14 @@
 		</a>
 	</div>
 </div*/?>
-<div class="products is-desktop-block" data-animation>
+<div class="products" data-animation>
 	<div class="products__in">
-		<div class="product _red">
+		<a href="#" class="product _red">
 			<div class="product__inner">
 				<div class="product__title">
-					Инвестиционное<br />
-					страхование жизни
+					<span>Инвестиционное<br />
+					страхование жизни</span>
+
 				</div>
 				<div class="product__bottom">
 					<div class="product__val">100%</div>
@@ -112,13 +113,13 @@
 			</div>
 			<div class="product__hidden middle" style="background-image: url('/static/theme/images/home/product/1.jpg');">
 				<div class="middle__inner">
-					<a href="#" class="btn _red">Узнать больше</a>
+					<div class="btn _red">Узнать больше</div>
 				</div>
 			</div>
-		</div>
-		<div class="product _brown">
+		</a>
+		<a href="#" class="product _brown">
 			<div class="product__inner">
-				<div class="product__title">Кредит наличными</div>
+				<div class="product__title"><span>Кредит наличными</span></div>
 				<div class="product__bottom">
 					<div class="product__val">под 9%</div>
 					<div class="product__text">
@@ -129,16 +130,15 @@
 			</div>
 			<div class="product__hidden middle" style="background-image: url('/static/theme/images/home/product/2.jpg');">
 				<div class="middle__inner">
-					<a href="#" class="btn _red">Узнать больше</a>
+					<div class="btn _red">Узнать больше</div>
 				</div>
 			</div>
-		</div>
-		<div class="product _gray">
+		</a>
+		<a href="#" class="product _gray">
 			<div class="product__inner">
 				<div class="product__title">
-					Индивидуальный<br />
-					инвестиционный<br />
-					счет
+					<span>Индивидуальный<br />
+					инвестиционный счет</span>
 				</div>
 				<div class="product__bottom">
 					<div class="product__val">13%</div>
@@ -150,9 +150,9 @@
 			</div>
 			<div class="product__hidden middle" style="background-image: url('/static/theme/images/home/product/2.jpg');">
 				<div class="middle__inner">
-					<a href="#" class="btn _red">Узнать больше</a>
+					<div class="btn _red">Узнать больше</div>
 				</div>
 			</div>
-		</div>
+		</a>
 	</div>
 </div>
