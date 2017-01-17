cca5e772c0d0e96ea9cacb9a1beee0322cd4082f
viktor <viktrgulakv@rambler.ru>
2017-01-17 17:12:23 +0300

    add:подсказка минимальной суммы перевода

--- static/includes/transfer.php
+++ static/includes/transfer.php
@@ -80,6 +80,7 @@
 					<div class="home__transfer-block-in">
 						<div class="home__transfer-sum">
 							<input type="text" name="amount" data-sum-mask placeholder="Сумма перевода" data-transfer-inp="main">
+							<div class="home__transfer-sum-text">минимальная сумма перевода 50 руб</div>
 						</div>
 						<div class="home__transfer-commission">
 							<div class="home__transfer-commission-title">Комиссия за перевод</div>
