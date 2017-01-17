edbab01cfe37da9bc603650b33d23c1a13e1e95b
viktor <viktrgulakv@rambler.ru>
2017-01-17 16:44:32 +0300

    add:Раскрытие информации в соответствии с Указанием ЦБ РФ

--- static/about/disclosure-of-information/decree.php
+++ static/about/disclosure-of-information/decree.php
@@ -23,101 +23,101 @@
 			</div>
 		</div>
 		<div class="b-blocks">
-			<div class="b-block _top-indent animation__top" data-animation>
+			<div class="b-block _top-indent decree animation__top" data-animation>
 				<div class="grid__inner">
-					<div class="decree">
-						<h3>Раскрытие информации в соответствии с Указанием Банка России
-							от 28.12.2015 N 3921-У »О составе, объеме, порядке и сроках раскрытия информации профессиональными участниками рынка ценных бумаг»
-						</h3>
-						<div class="decree__items">
-							<div class="decree__item">
-								<div class="decree__item-num">1</div>
-								<div class="decree__item-title">
-									Полное и сокращенное фирменное наименование профессионального участника рынка ценных бумаг,
-									в том числе на иностранном языке (при наличии двух последних)
-								</div>
-								<div class="decree__item-text">
-									<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
-									дата размещения: до 23.02.2016
-								</div>
+					<h3>Раскрытие информации в соответствии с Указанием Банка России
+						от 28.12.2015 N 3921-У «О составе, объеме, порядке и сроках раскрытия информации профессиональными участниками рынка ценных бумаг»
+					</h3>
+					<div class="decree__items">
+						<div class="decree__item">
+							<div class="decree__item-num">1</div>
+							<div class="decree__item-title">
+								Полное и сокращенное фирменное наименование профессионального участника рынка ценных бумаг,
+								в том числе на иностранном языке (при наличии двух последних)
 							</div>
-							<div class="decree__item selected">
-								<div class="decree__item-num">2</div>
-								<div class="decree__item-title">
-									Идентификационный номер налогоплательщика
-								</div>
-								<div class="decree__item-text">
-									<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
-									дата размещения: до 23.02.2016
-								</div>
-							</div>
-							<div class="decree__item">
-								<div class="decree__item-num">3</div>
-								<div class="decree__item-title">
-									Адрес профессионального участника рынка ценных бумаг, указанный в ЕГРЮЛ
-								</div>
-								<div class="decree__item-text">
-									<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
-									дата размещения: до 23.02.2016
-								</div>
-							</div>
-							<div class="decree__item selected">
-								<div class="decree__item-num">4</div>
-								<div class="decree__item-title">
-									Номер телефона, факса (при наличии последнего) профессионального участника рынка ценных бумаг
-								</div>
-								<div class="decree__item-text">
-									<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
-									дата размещения: до 23.02.2016
-								</div>
+							<div class="decree__item-text">
+								<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
+								дата размещения: до 23.02.2016
 							</div>
 						</div>
-						<h3>Рекомендуемая для размещения информация:</h3>
-						<div class="decree__items _pos2">
-							<div class="decree__item">
-								<div class="decree__item-num">3</div>
-								<div class="decree__item-title">
-									Порядок электронного документооборота профессионального участника рынка ценных бумаг (при наличии)
-								</div>
-								<div class="decree__item-text">
-									<a href="">http://rgsbank.ru/about/details/</a>, дата размещения: до 23.02.2016
-								</div>
+						<div class="decree__item selected">
+							<div class="decree__item-num">2</div>
+							<div class="decree__item-title">
+								Идентификационный номер налогоплательщика
 							</div>
-							<div class="decree__item selected">
-								<div class="decree__item-num">4</div>
-								<div class="decree__item-title">
-									Номер телефона, факса (при наличии последнего) профессионального участника рынка ценных бумаг
-								</div>
-								<div class="decree__item-text">
-									<p>Нет собственной системы электронного документооборота с клиентами.</p>
-									<p>ЭДО с НКО ЗАО НРД. Электронный документооборот осуществляется в соответствии с Правилами электронного документооборота НКО ЗАО НРД, являющимися приложением к Договору об обмене
-										электронными документами. Ознакомиться с Правилами электронного документооборота НКО ЗАО НРД можно на интернет-сайте компании (www.nsd.ru). Дата размещения: 24.06.2016г.</p>
-									<p>ЭДО с АО «Специализированный депозитарий „ИНФИНИТУМ“. Электронный документооборот осуществляется в соответствии с Правилами обмена
-										электронными документами в системе электронного документооборота ООО «Технический центр „ИНФИНИТУМ“ и Регламентом подсистемы электронного документооборота АО «Специализированный депозитарий „ИНФИНИТУМ“, на основании
-										заявления о присоединении к правилам системы электронного документооборота. Ознакомиться с Правилами электронного документооборота АО «Специализированный депозитарий „ИНФИНИТУМ“
-										можно на интернет-сайте компании (www.specdep.ru).
-										дата размещения: 24.06.2016г.</p>
+							<div class="decree__item-text">
+								<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
+								дата размещения: до 23.02.2016
 							</div>
+						</div>
+						<div class="decree__item">
+							<div class="decree__item-num">3</div>
+							<div class="decree__item-title">
+								Адрес профессионального участника рынка ценных бумаг, указанный в ЕГРЮЛ
 							</div>
-							<div class="decree__item">
-								<div class="decree__item-num">5</div>
-								<div class="decree__item-title">
-									Типовой регламент, определяющий порядок оказания услуг на рынке ценных бумаг
-									(иные формуляры или стандартные формы, определяющие условия договора (договоров) присоединения (далее — Регламент) (при наличии)
-								</div>
-								<div class="decree__item-text">
-									<p><a href="#">http://rgsbank.ru/about/details/</a>
-										Список документов/Клиентский регламент Депозитария</p>
-									<p><a href="#">http://rgsbank.ru/about/details/</a>
-										Список документов/Клиентский регламент Депозитария</p>
-									<p><a href="#">http://www.rgsbank.ru/financial/stock-market/</a>
-										дата размещения: до 23.02.2016</p>
-								</div>
+							<div class="decree__item-text">
+								<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
+								дата размещения: до 23.02.2016
+							</div>
+						</div>
+						<div class="decree__item selected">
+							<div class="decree__item-num">4</div>
+							<div class="decree__item-title">
+								Номер телефона, факса (при наличии последнего) профессионального участника рынка ценных бумаг
+							</div>
+							<div class="decree__item-text">
+								<a href="http://rgsbank.ru/about/details/">http://rgsbank.ru/about/details/</a>,
+								дата размещения: до 23.02.2016
+							</div>
+						</div>
+					</div>
+					<h3>Рекомендуемая для размещения информация:</h3>
+					<div class="decree__items _size2">
+						<div class="decree__item">
+							<div class="decree__item-num">3</div>
+							<div class="decree__item-title">
+								Порядок электронного документооборота профессионального участника рынка ценных бумаг (при наличии)
+							</div>
+							<div class="decree__item-text">
+								<a href="">http://rgsbank.ru/about/details/</a>, дата размещения: до 23.02.2016
+							</div>
+						</div>
+						<div class="decree__item selected">
+							<div class="decree__item-num">4</div>
+							<div class="decree__item-title">
+								Номер телефона, факса (при наличии последнего) профессионального участника рынка ценных бумаг
+							</div>
+							<div class="decree__item-text">
+								<p>Нет собственной системы электронного документооборота с клиентами.</p>
+								<p>ЭДО с НКО ЗАО НРД. Электронный документооборот осуществляется в соответствии с Правилами электронного документооборота НКО ЗАО НРД, являющимися приложением к Договору об обмене
+									электронными документами. Ознакомиться с Правилами электронного документооборота НКО ЗАО НРД можно на интернет-сайте компании (www.nsd.ru). Дата размещения: 24.06.2016г.</p>
+								<p>ЭДО с АО «Специализированный депозитарий „ИНФИНИТУМ“. Электронный документооборот осуществляется в соответствии с Правилами обмена
+									электронными документами в системе электронного документооборота ООО «Технический центр „ИНФИНИТУМ“ и Регламентом подсистемы электронного документооборота АО «Специализированный депозитарий „ИНФИНИТУМ“, на основании
+									заявления о присоединении к правилам системы электронного документооборота. Ознакомиться с Правилами электронного документооборота АО «Специализированный депозитарий „ИНФИНИТУМ“
+									можно на интернет-сайте компании (www.specdep.ru).
+									дата размещения: 24.06.2016г.</p>
+						</div>
+						</div>
+						<div class="decree__item">
+							<div class="decree__item-num">5</div>
+							<div class="decree__item-title">
+								Типовой регламент, определяющий порядок оказания услуг на рынке ценных бумаг
+								(иные формуляры или стандартные формы, определяющие условия договора (договоров) присоединения (далее — Регламент) (при наличии)
+							</div>
+							<div class="decree__item-text">
+								<p><a href="#">http://rgsbank.ru/about/details/</a>
+									Список документов/Клиентский регламент Депозитария</p>
+								<p><a href="#">http://rgsbank.ru/about/details/</a>
+									Список документов/Клиентский регламент Депозитария</p>
+								<p><a href="#">http://www.rgsbank.ru/financial/stock-market/</a>
+									дата размещения: до 23.02.2016</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="b-blocks _gray">
 			<?include "../../includes/news-gall.php"?>
 		</div>
 	</div>
