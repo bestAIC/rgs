20fc8b78e7c1bfdaf957d9412aed3a3b09fc4149
viktor <viktrgulakv@rambler.ru>
2017-01-18 17:13:58 +0300

    fix:упростил маску номера карты

--- static/theme/js/main.js
+++ static/theme/js/main.js
@@ -299,7 +299,8 @@ app.CardP2P = function () {
 		;
 	setMasks();
 	function setMasks() {
-		$transferBlock.find('[data-card-mask]').mask("0000 0000 0000 0000",{clearIfNotMatch: true});
+		//$transferBlock.find('[data-card-mask]').mask("0000 0000 0000 0000",{clearIfNotMatch: true});
+		$transferBlock.find('[data-card-mask]').mask("#");
 		$transferBlock.find('[data-cvv-mask]').mask("000",{clearIfNotMatch: true});
 		$transferBlock.find('[data-date-mask]').mask("A0/00",{
 			clearIfNotMatch: true,
