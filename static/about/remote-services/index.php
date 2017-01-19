9038a510062ab2b9e6a5c4fdc34e440cd74738d9
viktor <viktrgulakv@rambler.ru>
2017-01-19 18:12:30 +0300

    add:Дистанционные сервисы

---
+++ static/about/remote-services/index.php
@@ -0,0 +1,132 @@
+<?include "../../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in"  style="background-image: url('/static/theme/images/banners/about.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Дистанционные сервисы</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">О Банке</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Дистанционные сервисы</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block _top-indent animation__top remote-nav page-nav" data-animation>
+				<div class="grid__inner">
+					<div class="page-nav__row">
+						<div class="page-nav__row-col">
+							<a href="#" class="page-nav__item _big remote-nav__bank">
+								<div class="page-nav__item-title">
+									Интернет-Банк
+								</div>
+								<div class="page-nav__item-text">
+									Удобное и безопасное управление финансами
+									в режиме реального времени
+								</div>
+							</a>
+						</div>
+						<div class="page-nav__row-col _items">
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-title">
+									Мобильный Банк
+								</div>
+								<div class="page-nav__item-text">
+									Удобное мобильное приложение
+								</div>
+							</a>
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-title">
+									SMS-информирование
+								</div>
+								<div class="page-nav__item-text">
+									Оперативный контроль операций по счету
+									при помощи SMS-сообщений на мобильный телефон
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="page-nav__row">
+						<div class="page-nav__row-col">
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-title">
+									Обслуживание через банкоматы
+								</div>
+								<div class="page-nav__item-text">
+									Банковские операции и платежи без очередей
+									и посещения кассы
+								</div>
+							</a>
+						</div>
+						<div class="page-nav__row-col">
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-title">
+									Контакт-центр Банка
+								</div>
+								<div class="page-nav__item-text">
+									Круглосуточные консультации по продуктам
+									и услугам Банка.
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks">
+			<div class="b-block animation__top" data-animation>
+				<div class="deposit-info__block">
+					<div class="grid__inner">
+						<div class="inner__info-benefits">
+							<div class="inner__info-benefits-title">
+								Преимущества дистанционного банковского обслуживания
+							</div>
+							<ul class="b-list no-b-indent">
+								<li>Возможность управления счетами и картами из любой точки, где есть Интернет.</li>
+								<li>Круглосуточный доступ к банковским операциям;</li>
+								<li>Многообразие каналов дистанционного банковского обслуживания – Интернет,
+									круглосуточный Контакт-центр, более 450 собственных банкоматов по всей России;</li>
+								<li>Мгновенное выполнение большинства операций;</li>
+								<li>Льготные условия (или отсутствие комиссий) по оплате ряда услуг через дистанционные сервисы;</li>
+								<li>Безопасный доступ по защищенным каналам связи.</li>
+							</ul>
+						</div>
+						<div class="inner__info-block">
+							<div class="files">
+								<a href="#" class="file w100">
+									<div class="file__inner">
+										<div class="file__name">Список пунктов приема платежей</div>
+										<div class="file__props">XLS, 6.58MB</div>
+									</div>
+								</a>
+								<a href="#" class="file w100">
+									<div class="file__inner">
+										<div class="file__name">Список пунктов приема платежей</div>
+										<div class="file__props">XLS, 6.58MB</div>
+									</div>
+								</a>
+								<a href="#" class="file w100">
+									<div class="file__inner">
+										<div class="file__name">Список пунктов приема платежей</div>
+										<div class="file__props">XLS, 6.58MB</div>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "../../includes/news-gall.php"?>
+		</div>
+	</div>
+<?include "../../includes/footer.php"?>
+
