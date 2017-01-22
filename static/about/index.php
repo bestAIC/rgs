c6f8f2773fb8ac5f6b41c87122f2c353f1409e4e
viktor <viktrgulakv@rambler.ru>
2017-01-23 00:45:01 +0300

    add:о банке

---
+++ static/about/index.php
@@ -0,0 +1,430 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit about">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/about.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">О Банке</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Частным клиентам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>О Банке</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top about__info"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="numerals">
+						<div class="numerals__block">
+							<div class="numerals__item _big">
+								<div class="numerals__item-val">2<span>млн</span></div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									довольных клиентов
+								</div>
+							</div>
+						</div>
+						<div class="numerals__block">
+							<div class="numerals__item _small">
+								<div class="numerals__item-val"><span>топ</span> 60</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									среди крупнейших российских банков по чистым активам и капиталу
+								</div>
+							</div>
+							<div class="numerals__item _small">
+								<div class="numerals__item-val"><span>топ</span> 50</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									среди российских банков по различным показателям ритейла (вклады и кредиты частным лицам)
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						РОСГОССТРАХ БАНК (ПАО «РГС Банк») — универсальный коммерческий банк, основанный в 1994 году (лицензия № 3073 от 16.04.2015 г. ).<br/><br/>
+						Предоставляет все виды банковских услуг как физическим лицам с доходами различного уровня, так и корпоративным клиентам.
+							Клиентами Банка являются более 2 миллионов частных лиц и свыше 22 тысяч предприятий.
+					</div>
+					<div class="about__info-list">
+						<div class="about__info-list-title">Предоставляемые услуги:</div>
+						<ul class="b-list">
+							<li>потребительское кредитование;</li>
+							<li>консультации по инвестиционным продуктам;</li>
+							<li>покупка акций компании РОСГОССТРАХ;</li>
+							<li>оформление страховых полисов</li>
+						</ul>
+					</div>
+					<div class="about__info-list">
+						<div class="about__info-list-title">Для представителей малого и среднего бизнеса и корпоративных клиентов:</div>
+						<ul class="b-list">
+							<li>услуги кредитования и расчетно-кассового обслуживания;</li>
+							<li>размещения временно свободных средств;</li>
+							<li>зарплатные проекты;</li>
+							<li>международное финансирование и документарные операции;</li>
+							<li>гарантийные услуги и др.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="grid__inner">
+				<div class="about__info-numerals">
+					<div class="about__info-numerals-inner">
+						<div class="about__info-numerals-item _1">
+							<div class="about__info-numerals-item-val">75</div>
+							<div class="about__info-numerals-item-text">регионов
+								России</div>
+						</div>
+						<div class="about__info-numerals-item _2">
+							<div class="about__info-numerals-item-val">>20</div>
+							<div class="about__info-numerals-item-text">лет на рынке</div>
+						</div>
+						<div class="about__info-numerals-item _3">
+							<div class="about__info-numerals-item-val">1700</div>
+							<div class="about__info-numerals-item-text">точек продаж</div>
+						</div>
+						<div class="about__info-numerals-item _4">
+							<div class="about__info-numerals-item-val">>400</div>
+							<div class="about__info-numerals-item-text">устройств собственной банкоматной сети</div>
+						</div>
+						<div class="about__info-numerals-item _5">
+							<div class="about__info-numerals-item-val">2<span>-я</span></div>
+							<div class="about__info-numerals-item-text">по размерам сеть банковских отделений в стране</div>
+						</div>
+					</div>
+				</div>
+				<div class="about__associations">
+					<div class="about__associations-title">Банк является членом следующих ассоциаций:</div>
+					<div class="about__associations-block">
+						<div class="about__associations-img"></div>
+						<div class="about__associations-text">
+							Конкурентные позиции Банка ежегодно подтверждаются присвоенными рейтингами крупнейших
+							российских и международных рейтинговых агентств, а также периодическими рэнкингами, публикуемыми в СМИ.<br/><br/>
+							Банк входит в Систему страхования вкладов.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Миссия и ценности</h2>
+					<div class="about__mission">
+						<strong>РОСГОССТРАХ БАНК</strong> – это Банк общероссийского масштаба, часть финансовой группы РОСГОССТРАХ,
+						основным направлением деятельности которой является предоставление банковских услуг для розничных клиентов и предприятий среднего и малого бизнеса.
+						<h4>Мы:</h4>
+						<ul class="b-list">
+							<li>ценим и любим своих Клиентов;</li>
+							<li>предоставляем Клиентам простые и удобные продукты и услуги;</li>
+							<li>помогаем своим Клиентам изменить жизнь к лучшему;</li>
+							<li>заботимся о своих сотрудниках, создавая компанию и формируя команду, в которой хочется работать;</li>
+							<li>не стоим на месте, динамично развиваясь и меняясь к лучшему.</li>
+						</ul>
+						<div class="about__principles">
+							<h4>Ценности и принципы работы</h4>
+							<div class="about__principles-blocks">
+								<div class="about__principles-block">
+									<p><strong>Честность</strong> - наш главный принцип.</p>
+									<p><strong>Ценности</strong> - это корень нашей силы, основа наших решений, мотив наших действий и источник наших стремлений:</p>
+									<ul class="b-list about__principles-list">
+										<li><strong>Развитие</strong></li>
+										<li><strong>Команда</strong></li>
+										<li><strong>Неравнодушие</strong></li>
+										<li><strong>Уважение</strong></li>
+										<li><strong>Трудолюбие</strong></li>
+									</ul>
+								</div>
+								<div class="about__principles-block">
+									<p><strong>Наша стратегическая цель</strong> — сделать из РОСГОССТРАХ БАНКА компанию, которая из года в год будет:</p>
+									<ul class="b-list">
+										<li>эталоном надежности и качества обслуживания для клиентов в банковской отрасли;</li>
+										<li>эталоном качества работы сотрудников и качества управления людьми и процессами;</li>
+										<li>эталоном работодателя для сотрудников в финансовой отрасли.</li>
+									</ul>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="about__controls">
+			<div class="b-block animation__top" data-animation data-tabs>
+				<div class="remote-services__tabs no-mob-block">
+					<div class="grid__inner">
+						<h2 >Органы управления:</h2>
+						<a href="javascript:void(0);" data-tabs-tab="1" class="faq__tab _active">Совет директоров</a>
+						<a href="javascript:void(0);" data-tabs-tab="2" class="faq__tab">Правление</a>
+						<a href="javascript:void(0);" data-tabs-tab="3" class="faq__tab">Акционеры и афиллированные лица</a>
+					</div>
+				</div>
+				<div class="remote-services__tabs is-mob-block">
+					<div class="grid__inner">
+						<h2 >Пакеты услуг:</h2>
+					</div>
+					<div class="swiper-container" data-faq-mob-tabs>
+						<div class="swiper-wrapper">
+							<div class="header__menu-item swiper-slide">
+								<a href="javascript:void(0);"  data-tabs-tab="1" class="faq__tab _active">Совет директоров</a>
+							</div>
+							<div class="header__menu-item swiper-slide">
+								<a href="javascript:void(0);"  data-tabs-tab="2" class="faq__tab">Правление</a>
+							</div>
+							<div class="header__menu-item swiper-slide">
+								<a href="javascript:void(0);"  data-tabs-tab="3" class="faq__tab">Акционеры и афиллированные лица</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div data-tabs-content="1">
+					<div class="grid__inner">
+						<div class="about__controls-team">
+							<div class="about__controls-team-text">
+								Осуществляет общее руководство деятельностью Банка, за исключением вопросов, отнесенных к исключительной компетенции Общего собрания акционеров,
+								определяет стратегическое направление деятельности Банка
+							</div>
+							<div class="about__controls-team-items">
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Клепальская</div>
+										Светлана Владимировна
+									</div>
+									<div class="about__controls-team-item-position">Председатель Совета Директоров</div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Фалев</div>
+										Александр Васильевич
+									</div>
+									<div class="about__controls-team-item-position"></div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Майоров</div>
+										Константин Викторович
+									</div>
+									<div class="about__controls-team-item-position"></div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Харчилава</div>
+										Хвича Патаевич
+									</div>
+									<div class="about__controls-team-item-position"></div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Щербаченко</div>
+										Петр Сергеевич
+									</div>
+									<div class="about__controls-team-item-position"></div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Пухова</div>
+										Марина Михайловна
+									</div>
+									<div class="about__controls-team-item-position"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				<div data-tabs-content="2" class="hide">
+					<div class="grid__inner">
+						<div class="about__controls-team">
+							<div class="about__controls-team-text">
+								Осуществляет текущее руководство деятельностью Банка и отвечает за   реализацию целей, стратегии и политики Банка, определяемых Общим собранием акционеров
+								и Советом директоров Банка.
+							</div>
+							<div class="about__controls-team-items">
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Масленникова</div>
+										Алла Александровна
+									</div>
+									<div class="about__controls-team-item-position">Председатель Правления</div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Алоян</div>
+										Артур Рафаелович
+									</div>
+									<div class="about__controls-team-item-position">
+										член Правления, Первый Заместитель
+										Председателя Правления
+									</div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Еникеев</div>
+										Аскер Джевадович
+									</div>
+									<div class="about__controls-team-item-position">
+										член Правления, Заместитель
+										Председателя Правления
+									</div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Тараканова</div>
+										Ирина Борисовна
+									</div>
+									<div class="about__controls-team-item-position">член Правления, Главный бухгалтер</div>
+								</div>
+								<div class="about__controls-team-item">
+									<div class="about__controls-team-item-name">
+										<div class="surname">Павлова</div>
+										Лариса Юрьевна
+									</div>
+									<div class="about__controls-team-item-position">
+										член Правления, Руководитель службы
+										внутреннего контроля и комплаенса
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div data-tabs-content="3" class="hide">
+					<div class="grid__inner">
+						<div class="about__controls-block">
+							<div class="about__controls-block-right">
+								<div class="about__controls-numerals">
+									<div class="about__controls-numerals-val">2 077 432 500 <span class="rub"></span></div>
+									<div class="about__controls-numerals-text">Уставный капитал<br />РОСГОССТРАХ БАНКА</div>
+								</div>
+							</div>
+							<div class="about__controls-block-left">
+								<div class="about__controls-title">Сведения об уставном капитале и акционерах банка</div>
+								<div class="about__controls-block-content">
+									Уставный капитал РОСГОССТРАХ БАНКА составляет
+									2 077 432 500 рублей (общая номинальная стоимость обыкновенных бездокументарных именных акций 2 074 102 500 рублей — 99,84% УК Банка, общая номинальная стоимость привилегированных именных бездокументарных акций
+									3 330 000 рублей — 0,16% УК Банка).
+								</div>
+							</div>
+						</div>
+						<div class="about__controls-block">
+							<div class="about__controls-block-right">
+								<div class="about__controls-title">Привилегированные именные бездокументарные акции:</div>
+								<ul class="b-list">
+									<li>государственный регистрационный номер 10203073В;</li>
+									<li>номинальная стоимость 500 рублей;</li>
+									<li>количество размещенных акций 6 660 шт.</li>
+								</ul>
+							</div>
+							<div class="about__controls-block-left">
+								<div class="about__controls-title">Обыкновенные бездокументарные именные акции:</div>
+								<div class="about__controls-block-content">
+									<ul class="b-list">
+										<li>государственный регистрационный номер 10103073В;</li>
+										<li>номинальная стоимость 500 рублей;</li>
+										<li>количество размещенных акций 4 148 205 шт.</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="about__controls-block">
+							<div class="about__controls-block-left">
+								Весь пакет привилегированных именных бездокументарных акций Банка находится в собственности ООО "Финансово-Инвестиционная Компания".
+							</div>
+						</div>
+						<div class="about__shareholders">
+							<h3>Акционеры, владеющие долей свыше 1% акций Банка</h3>
+							<table class="deposit-table about__shareholders-table">
+								<tr class="deposit-table__titles">
+									<td>АКЦИОНЕРЫ БАНКА</td>
+									<td class="center">Процент к общему количеству голосующих (обыкновенных) акций</td>
+									<td  class="center">Процент к общему количеству акций</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>RGS CAPITAL LIMITED (РГС КАПИТАЛ ЛИМИТЕД)</td>
+									<td class="center">23,8804</td>
+									<td class="center">23,8422</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ООО «ИК «Т.А.Р.Г.Е.Т. Капитал»</td>
+									<td class="center">21,1076</td>
+									<td class="center">21,0738</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ПАО СК «Росгосстрах»</td>
+									<td class="center">14,6192</td>
+									<td class="center">14,5958</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ООО «Финансово-Инвестиционная Компания»</td>
+									<td class="center">13,9191</td>
+									<td class="center">14,0570</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ООО «СК «РГС-Жизнь»</td>
+									<td class="center">9,8999</td>
+									<td class="center">9,8840</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ООО «РГСН»</td>
+									<td class="center">10,2135</td>
+									<td class="center">1,2973</td>
+								</tr>
+								<tr class="deposit-table__row">
+									<td>ООО «Лингвист»</td>
+									<td class="center">1,2993</td>
+									<td class="center">1,2973</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="about__affiliated">
+						<div class="grid__inner">
+							<h3>Список аффилированных лиц:</h3>
+							<div class="docs annual-reports">
+								<?for ($i=0;$i<4;$i++){?>
+									<div class="deposit-doc" data-nav-block>
+										<a href="javascript:void(0);" data-nav-btn class="deposit-doc__title">Изменения, произошедшие в списке аффилированных лиц за 2016 год</a>
+										<div class="deposit-doc__content hide" data-nav-content>
+											<a href="#" class="file">
+												<div class="file__inner">
+													<div class="file__name">Годовая бухгалтерская отчетность за 2015 год (в соответствии с РСБУ), подтвержденная аудиторами</div>
+													<div class="file__props">PDF, 450.96KB</div>
+												</div>
+											</a>
+											<a href="#" class="file">
+												<div class="file__inner">
+													<div class="file__name">Тарифы по оплате услуг, предоставляемых физическим лицам</div>
+													<div class="file__props">PDF, 450.96KB</div>
+												</div>
+											</a>
+											<a href="#" class="file">
+												<div class="file__inner">
+													<div class="file__name">Тарифы по оплате услуг, предоставляемых физическим лицам</div>
+													<div class="file__props">PDF, 450.96KB</div>
+												</div>
+											</a>
+										</div>
+									</div>
+								<?}?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
