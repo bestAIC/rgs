37bf3fe1b245784737a96d71ec11696e53a63e96
viktor <viktrgulakv@rambler.ru>
2017-02-01 11:29:33 +0300

    upd: архив документов

---
+++ static/about/disclosure-of-information/docs.php
@@ -0,0 +1,58 @@
+<?include "../../includes/header.php"?>
+	<div class="inner">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/about.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Информация для акционеров и иных заинтересованных лиц</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Частным клиентам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">О Банке</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Информация для акционеров и иных заинтересованных лиц</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block _top-indent animation__top" data-animation>
+				<div class="grid__inner">
+					<div class="files-list">
+						<?for($i=1;$i<=7;$i++){?>
+							<a href="#" class="file">
+								<div class="file__inner">
+									<div class="file__name">Уведомление о проведении внеочередного общего собрания акционеров Публичного акционерного общества «Росгосстрах Банк» (на 20.10.2016 г.)</div>
+									<div class="file__props">(XLS, 35KB)</div>
+								</div>
+							</a>
+						<?}?>
+					</div>
+					<div class="archive" data-nav-block>
+						<div class="gold-link__wrap">
+							<a href="javascript:void(0)" data-nav-btn class="gold-link _arr-down">Архив</a>
+						</div>
+						<div class="files-list hide" data-nav-content>
+							<?for($i=1;$i<=7;$i++){?>
+								<a href="#" class="file">
+									<div class="file__inner">
+										<div class="file__name">Уведомление о проведении внеочередного общего собрания акционеров Публичного акционерного общества «Росгосстрах Банк» (на 20.10.2016 г.)</div>
+										<div class="file__props">(XLS, 35KB)</div>
+									</div>
+								</a>
+							<?}?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?include "../../includes/news-gall.php"?>
+		</div>
+	</div>
+<?include "../../includes/footer.php"?>
+
