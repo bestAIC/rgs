b65e269fbf60977509ba4a212dff0f92b8aa5ad7
viktor <viktrgulakv@rambler.ru>
2017-02-17 17:28:56 +0300

    add:Операции на фондовом рынке

---
+++ static/financial/stock-market.php
@@ -0,0 +1,122 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/financial.png');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Финансовым институтам</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Финансовым институтам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Операции на фондовом рынке</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="base-info deposit-info__block">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Управление межбанковских отношений:</div>
+								<div class="base-info__block-text">
+									(495) 925 80 60, доб. 0771747, 0771760
+								</div>
+							</div>
+							<div class="base-info__block">
+								<div class="base-info__block-title">Управление операций на финансовых рынках:</div>
+								<div class="base-info__block-text">
+									(495) 925 80 68, доб. 0771786
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__block block-indent is-desktop-block">
+						<h3 >Список документов</h3>
+						<div class="files">
+							<a href="#" class="file w100">
+								<div class="file__inner">
+									<div class="file__name">Вопросник для физических лиц</div>
+									<div class="file__props">DOC, 70.42KB</div>
+								</div>
+							</a>
+							<a href="#" class="file w100">
+								<div class="file__inner">
+									<div class="file__name">Вопросник для физических лиц</div>
+									<div class="file__props">DOC, 70.42KB</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						РОСГОССТРАХ БАНК с 2008 года является профессиональным участником фондового рынка, имеет лицензии Федеральной
+						Службы по Финансовым Рынкам (ФСФР) на осуществление дилерской, брокерской и депозитарной деятельности
+					</div>
+					<div class="stock-market">
+						<h3>Услуги Банка на рынке ценных бумаг</h3>
+						<div class="stock-market__text">
+							Основными задачами данного направления являются укрепление деловых и партнерских отношений с финансовыми клиентами и контрагентами банка. Развивая взаимовыгодное сотрудничество, Банк постоянно увеличивает
+							количество партнеров
+						</div>
+						<ul class="b-list">
+							<li>
+								Рынок акций:
+								<ul class="dash-list">
+									<li>купля-продажа акций российских эмитентов</li>
+									<li>операции РЕПО с акциями российских эмитентов на биржевом и внебиржевом рынках</li>
+								</ul>
+							</li>
+							<li>
+								Рынок облигаций российских эмитентов:
+								<ul class="dash-list">
+									<li>купля-продажа облигаций российских эмитентов (государственные, субфедеральные, муниципальные и корпоративные займы)</li>
+									<li>операции РЕПО на биржевом и внебиржевом рынка</li>
+								</ul>
+							</li>
+							<li>
+								Рынок еврооблигаций:
+								<ul class="dash-list">
+									<li>покупка/продажа суверенных и корпоративных еврооблигаций РФ, стран СНГ и российских корпоративных эмитентов</li>
+									<li>операции РЕПО с еврооблигациями</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="deposit-info__block block-indent no-desktop-block">
+					<h3 >Список документов</h3>
+					<div class="files">
+						<a href="#" class="file w100">
+							<div class="file__inner">
+								<div class="file__name">Вопросник для физических лиц</div>
+								<div class="file__props">DOC, 70.42KB</div>
+							</div>
+						</a>
+						<a href="#" class="file w100">
+							<div class="file__inner">
+								<div class="file__name">Вопросник для физических лиц</div>
+								<div class="file__props">DOC, 70.42KB</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
