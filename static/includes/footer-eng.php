a4e1b74b0dda4f49d4b659ee563b00eb93ef30ca
viktor <viktrgulakv@rambler.ru>
2017-02-17 10:51:12 +0300

    upd:eng version

---
+++ static/includes/footer-eng.php
@@ -0,0 +1,46 @@
+<div class="loader js-loader"><div class="loader_box"></div></div>
+</div>
+<footer class="footer footer-eng">
+	<div class="grid__inner">
+		<div class="footer-eng__contacts">
+			<a href="tel:8 495 925 8060" class="footer-eng__phone">8 495 925 8060</a>
+			<div class="footer-eng__social">
+				<a href="https://twitter.com/rgs_bank" target="_blank" class="footer__social-item _tw"></a>
+				<a href="https://www.facebook.com/rgsbank" target="_blank" class="footer__social-item _fb"></a>
+			</div>
+		</div>
+		<div class="footer-eng__main">
+			<div class="footer-eng__menu">
+				<div class="footer-eng__menu-col">
+					<div class="footer-eng__menu-item">
+						<a href="#">Careers</a>
+					</div>
+					<div class="footer-eng__menu-item">
+						<a href="#">Sitemap</a>
+					</div>
+				</div>
+				<div class="footer-eng__menu-col">
+					<div class="footer-eng__menu-item">
+						<a href="#">Contacts</a>
+					</div>
+					<div class="footer-eng__menu-item">
+						<a href="#">Inside information</a>
+					</div>
+				</div>
+			</div>
+			<div class="footer-eng__block">
+				<a href="/" class="footer__logo"></a>
+				<div class="footer__license">
+					© 2012-<?=date('Y')?>, Rosgosstrakh Bank <span class="is-desktop-block"><br/></span>
+					General License of Bank of Russia № 3073.
+				</div>
+			</div>
+		</div>
+	</div>
+
+</footer>
+<script src="/static/theme/js/libs.js"></script>
+<script src="/static/theme/js/main.js"></script>
+
+</body>
+</html>
