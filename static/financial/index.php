85bcd7f7702d45b10d27bee2777bc4a20b95c0d4
viktor <viktrgulakv@rambler.ru>
2017-02-17 16:16:49 +0300

    add:финансовым институтам

---
+++ static/financial/index.php
@@ -0,0 +1,107 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/financial.png');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Финансовым институтам</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Финансовым институтам</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="base-info">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Управление межбанковских отношений</div>
+								<div class="base-info__block-text">
+									(495) 925 80 60 &nbsp;&nbsp;доб. 077 1286
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						Последние 15 лет РОСГОССТРАХ БАНК является активным участником межбанковского рынка
+					</div>
+					<div class="inner__info-benefits-text _big">
+						<p>Основными задачами данного направления являются укрепление деловых и партнерских отношений с
+						финансовыми клиентами и контрагентами банка. Развивая взаимовыгодное сотрудничество,
+						Банк постоянно увеличивает количество партнеров</p>
+
+						<p>Кредитоспособность и финансовая устойчивость банка подтверждена
+							<a href="#">ведущими рейтинговыми агентствами</a></p>
+
+						<p>Мы всегда открыты для сотрудничества и готовы предоставить широкий спектр услуг по
+						основным направлениям межбанковского бизнеса</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top financial-nav page-nav" data-animation>
+				<div class="grid__inner">
+					<div class="page-nav__row">
+						<div class="page-nav__row-col">
+							<a href="#" class="page-nav__item _big financial-nav__market">
+								<div class="page-nav__item-title">
+									Операции на денежном рынке
+								</div>
+								<div class="page-nav__item-text">
+									Привлечение денежных средств, краткосрочное кредитование конверсионные операции...
+								</div>
+							</a>
+						</div>
+						<div class="page-nav__row-col _items">
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-ico _base _depository"></div>
+								<div class="page-nav__item-title">
+									Операции на фондовом рынке
+								</div>
+								<div class="page-nav__item-text">
+									Акции, еврооблигации и облигации
+									российских эмитентов
+								</div>
+							</a>
+							<a href="#" class="page-nav__item">
+								<div class="page-nav__item-ico _base _docs"></div>
+								<div class="page-nav__item-title">
+									Корреспондентские отношения
+								</div>
+								<div class="page-nav__item-text">
+									Индивидуальный подход к каждому контрагенту <span class="no-tablet-block">и современные технологии проведения межбанковских операций</span>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="page-nav__row">
+						<a href="#" class="page-nav__item financial-nav__depository">
+							<div class="financial-nav__depository-ico"></div>
+							<div class="page-nav__item-title">
+								Депозитарий
+							</div>
+							<div class="page-nav__item-text">
+								Готовые инвестиционные решения на основе Российских и Зарубежных ценных бумаг
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
