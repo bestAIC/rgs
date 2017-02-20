2698d41b0b58793f5a7c909e74fbe0afe64202e6
viktor <viktrgulakv@rambler.ru>
2017-02-20 13:05:38 +0300

    add:Корреспондентские отношения

---
+++ static/financial/correspondent.php
@@ -0,0 +1,113 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/financial.png');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Корреспондентские отношения</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Финансовым институтам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Корреспондентские отношения</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="deposit__info-banner _top-text deposit-info__block">
+						<div class="deposit__info-banner-in">
+							<div class="deposit__info-banner-title correspondent__banner-title">Бесплатно предоставим кредитовые и дебетовые авизо по корреспондентским счетам</div>
+						</div>
+						<div class="deposit__info-banner-img"  style="background-image: url(<?=$desktopAndTableLand ? '/static/theme/images/info-banners/credit/Mortgage_pic.png'
+							: ($tablePort ? '/static/theme/images/info-banners/credit/Mortgage_tablet_pic.png':'/static/theme/images/info-banners/credit/Mortgage_mobile.png') ?>)"></div>
+					</div>
+					<div class="base-info deposit-info__block correspondent__contacts">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Управление межбанковских отношений:</div>
+								<div class="base-info__block-text">
+									(495) 925 80 60, доб. 0771747, 0771760
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits  correspondent__benefits">
+					<div class="inner__info-benefits-title">
+						<div class="correspondent__benefits-title">
+							Многолетний опыт РОСГОССТРАХ БАНК в сфере
+							предоставления расчетных услуг банкам-респондентам позволяет Банку предлагать кредитным организациям одни из
+							 	наиболее привлекательных условий открытия и ведения корреспондентских счетов как в рублях РФ, так и в иностранных валютах.
+						</div>
+						Преимущества открытия корреспондентских
+						счетов в РОСГОССТРАХ БАНК
+					</div>
+					<ul class="b-list" data-nav-block>
+						<li>бесплатное открытие и ведение корреспондентских счетов;</li>
+						<li>возможность проведения всего спектра расчетных операций в рублях РФ и свободно конвертируемых валютах (СКВ);</li>
+						<li>отсутствие минимально требуемой суммы кредитового остатка на корреспондентском счете для начисления процентов на текущие остатки по счетам;</li>
+						<li>начисление процентов на неснижаемые остатки денежных средств, образуемые на корреспондентских счетах (бронирование), в том числе на условиях «овернайт»;</li>
+						<li>зачисление денежных средств на корреспондентские счета по входящим платежам без взимания комиссии;</li>
+						<li data-nav-content class="hide">зачисление денежных средств на корреспондентские счета по входящим платежам без взимания комиссии;</li>
+						<li data-nav-content class="hide">зачисление денежных средств на корреспондентские счета по входящим платежам без взимания комиссии;</li>
+						<li data-nav-content class="hide">зачисление денежных средств на корреспондентские счета по входящим платежам без взимания комиссии;</li>
+						<div class="gold-link__wrap correspondent__benefits-more">
+							<a href="javascript:void(0)" data-nav-btn class="gold-link _arr-down"><span  data-nav-text>Все преимущества</span><span data-nav-text class="hide">Свернуть</span></a>
+						</div>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Порядок обслуживания</h2>
+					<div class="correspondent__service">
+						<div class="correspondent__service-block">
+							<div class="correspondent__service-block-title">В иностранной валюте</div>
+							<ul class="b-list">
+								<li>
+									Исполнение платежей:
+									<ul class="dash-list">
+										<li>в долларах США, поступивших в Банк до 17:00 по московскому времени, текущим днем валютирования, после 17:00 московского времени — следующим днем валютирования;</li>
+										<li>ЕВРО, поступивших в банк до 16:00 московского времени, исполняются текущим днем валютирования, после 16:00 московского времени — следующим днем валютирования;</li>
+										<li>в остальных мировых валютах расчеты осуществляются вторым рабочим днем валютирования;</li>
+									</ul>
+								</li>
+								<li>внутренние списания / зачисления — до 17:30;</li>
+								<li>предоставление кредитовых документов текущим днем по мере поступления;</li>
+								<li>изменение условий платежа, розыск, возврат недошедших до получателя сумм;</li>
+								<li>использование любого удобного вида телекоммуникационной связи (TELEX, SWIFT, «Клиент-Банк» и других).</li>
+							</ul>
+						</div>
+						<div class="correspondent__service-block">
+							<div class="correspondent__service-block-title">В российских рублях</div>
+							<ul class="b-list">
+								<li>Операционный день: до 16:00 по московскому времени, включающий в себя прием и исполнение платежных поручений со сроком исполнения
+									«день в день»;</li>
+								<li>внутренние списания / зачисления: до 17:30;</li>
+								<li>ведение корреспондентского счета в режиме «онлайн»;</li>
+								<li>предоставление кредитовых документов текущим днем по мере поступления;</li>
+								<li>использование любого удобного вида телекоммуникационной связи (TELEX, SWIFT,
+									«Клиент-Банк»).</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
