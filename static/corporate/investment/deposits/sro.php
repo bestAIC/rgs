210f0dc7cc1a57ffb1b690876e0f2ba036343a29
viktor <viktrgulakv@rambler.ru>
2017-03-17 10:45:35 +0300

    add:Пополняемый депозит для СРО

---
+++ static/corporate/investment/deposits/sro.php
@@ -0,0 +1,257 @@
+<?include "../../../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/investment/deposit.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Пополняемый депозит для СРО</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Размещение свободных средств</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Депозиты</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Пополняемый депозит для СРО</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top _small-indent"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="numerals">
+						<div class="numerals__block">
+							<div class="numerals__item _big">
+								<div class="numerals__item-val">7,25%</div>
+								<div class="numerals__item-dash"></div>
+								<div class="numerals__item-text">
+									Максимальная ставка привлечения, % годовых
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title">
+						Пополняемый депозит для саморегулируемых организаций с правом досрочного
+						востребования части размещенной суммы и выплатой процентов в конце срока
+					</div>
+					<ul class="b-list">
+						<li>Пополнение возможно не позднее, чем за 30 дней
+							до окончания срока депозита</li>
+					</ul>
+					<div class="inner__info-benefits-btn">
+						<a href="javascript:void(0);" data-goto=".subscription" class="btn _red">Открыть депозит</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks">
+			<div class="b-block animation__top deposit-info" data-animation>
+				<div class="grid__inner">
+					<div class="deposit-info__left">
+						<div class="deposit-info__block">
+							<h3>Общая информация</h3>
+							<div class="deposit-conditions">
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Валюта депозита</div>
+									<div class="deposit-conditions__item-block">рубли</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Пополнение депозита</div>
+									<div class="deposit-conditions__item-block">
+										минимальная сумма пополнения 300 000 рублей
+									</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Срок размещения</div>
+									<div class="deposit-conditions__item-block">минимальный − 31 день, максимальный − 732 дня</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Расходование средств</div>
+									<div class="deposit-conditions__item-block">возможно до минимальной суммы размещения</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Выплата процентов</div>
+									<div class="deposit-conditions__item-block">в конце срока</div>
+								</div>
+								<div class="deposit-conditions__item">
+									<div class="deposit-conditions__item-block">Процентная ставка при досрочном расторжении: 	</div>
+									<div class="deposit-conditions__item-block">0,50%</div>
+								</div>
+							</div>
+						</div>
+						<div class="deposit-info__block is-mob-block">
+							<div class="deposit-scale">
+								<h3>Процентные ставки</h3>
+								<div class="deposit-scale__block">
+									<div class="deposit-scale__content">
+										<div class="deposit-scale__table">
+											<table>
+												<tr class="deposit-scale__table-titles">
+													<td>СУММА<br/>ВКЛАДА</td>
+													<td>от 500 000<br/>до 50 000 000</td>
+													<td>от 50 000 000</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td class="red">31-60</td>
+													<td>6,75%</td>
+													<td>7,00%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td class="red">61-90</td>
+													<td>6,75%</td>
+													<td>7,00%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">91 - 120</td>
+													<td>7,00%</td>
+													<td>7,25%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">121 - 150</td>
+													<td>7,00%</td>
+													<td>7,25%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">151 - 180</td>
+													<td>7,00%</td>
+													<td>7,25%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">181 - 270</td>
+													<td>7,00%</td>
+													<td>7,25%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">271 - 366</td>
+													<td>7,00%</td>
+													<td>7,25%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">367 - 545</td>
+													<td>6,50%</td>
+													<td>6,75%</td>
+												</tr>
+												<tr class="deposit-scale__table-row">
+													<td  class="red">546 - 732</td>
+													<td>6,00%</td>
+													<td>7,65%</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__right">
+						<div class="deposit-open deposit-info__block">
+							<h3>Как оформить депозит?</h3>
+							<div class="deposit-open__methods">
+								<div class="deposit-open__method">
+									<div class="deposit-open__method-content">
+										<div class="deposit-open__steps _w100">
+											<div class="deposit-open__step">
+												<div class="deposit-open__step-num">01</div>
+												Подготовьте <a href="#">необходимый пакет документов</a> для открытия депозита
+											</div>
+											<div class="deposit-open__step">
+												<div class="deposit-open__step-num">02</div>
+												Обратитесь в любое удобное для Вас <a href="#">отделение РОСГОССТРАХ БАНКА, где производится обслуживание юридических лиц</a>
+											</div>
+											<div class="deposit-open__step">
+												<div class="deposit-open__step-num">03</div>
+												Подпишите договор  о вкладе с Банком
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="deposit-open deposit-info__block">
+							<h3>Обратите внимание:</h3>
+							<div class="deposit-open__methods">
+								<div class="deposit-open__method">
+									<div class="deposit-open__method-content">
+										В течение срока действия депозита Клиент вправе досрочно востребовать часть депозита при условии
+										сохранения остатка в размере первоначального взноса и при предоставлении документа (решения уполномоченного органа),
+										подтверждающего необходимость осуществления выплаты за счет компенсационного фонда, при этом
+										Клиент обязан уведомить об этом Банк не менее чем за 30 (тридцать) рабочих дней до даты предполагаемого востребования
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="grid__inner no-mob-block">
+					<div class="deposit-scale">
+						<h3>Процентные ставки</h3>
+						<div class="deposit-table__wrap">
+							<table class="deposit-table _rowspan-table _reverse-indents">
+								<tr>
+									<th colspan="2" class="_left">Срок привлечения (дней)</th>
+									<th>31-60</th>
+									<th>61-90</th>
+									<th>91 - 120</th>
+									<th>121 - 150</th>
+									<th>151 - 180</th>
+									<th>181 - 270</th>
+									<th>271 - 366</th>
+									<th>367 - 545</th>
+									<th>546 - 732</th>
+								</tr>
+								<tr class="deposit-table__titles">
+									<td >ВАЛЮТА ВКЛАДА</td>
+									<td>СУММА депозита</td>
+									<td colspan="9" class="center">максимальная ставка привлечения, %</td>
+								</tr>
+								<tr class="deposit-table__row _grey">
+									<td class="bold valuta center"><span class="is-desktop-block"></span><span class="rub"></span></td>
+									<td >от 500 000 <nobr>до 50 000 000</nobr></td>
+									<td class="center bold">6,75</td>
+									<td class="center bold">6,75</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">6,50</td>
+									<td class="center bold">6,00</td>
+								</tr>
+								<tr class="deposit-table__row _grey">
+									<td></td>
+									<td >от 50 000 000</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,00</td>
+									<td class="center bold">7,25</td>
+									<td class="center bold">7,25</td>
+									<td class="center bold">7,25</td>
+									<td class="center bold">7,25</td>
+									<td class="center bold">7,25</td>
+									<td class="center bold">6,75</td>
+									<td class="center bold">7,65</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<?include "../../../includes/subscription.php"?>
+		</div>
+	</div>
+<?include "../../../includes/footer.php"?>
+
