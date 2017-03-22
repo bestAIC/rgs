a107a86c95cd262f777be1cb783e25d94917d2d1
viktor <viktrgulakv@rambler.ru>
2017-03-22 15:46:00 +0300

    Add:Корпоративные банковские карты

---
+++ static/corporate/cards.php
@@ -0,0 +1,173 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit salary-project">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/cards.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Корпоративные<br/>банковские карты</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Корпоративные банковские карты</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top salary-project__info"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title deposit-info__block">
+						<p>
+							Банк предлагает своим корпоративным клиентам услугу по выдаче уполномоченным
+							представителям предприятия международных расчетных банковских карт Visa и MasterCard
+						</p>
+						<p style="font-weight: 400;">
+							В целях проведения расчетов связанных с хозяйственной деятельностью, в том числе с оплатой командировочных
+							и представительских расходов, сотрудники предприятий с использованием корпоративных карт могут осуществлять следующие операции: 
+						</p>
+						<ul class="b-list">
+							<li>Получение наличных денежных средств в валюте РФ в банкоматах или кассах офисов Банка</li>
+							<li>Проведение операций по безналичной оплате товаров и услуг в торгово-сервисных предприятиях</li>
+							<li>Иные операции в валюте РФ и иностранной валюте с соблюдением требований законодательства, в том числе нормативных актов Банка России</li>
+						</ul>
+					</div>
+				</div>
+				<div class="inner__info-block">
+					<div class="deposit-open__method deposit-info__block">
+						<div class="deposit-open__method-title"><a href="#">Карты</a></div>
+						<div class="deposit-open__method-content">
+							Посмотрите виды карт, предлагаемых
+							РОСГОССТРАХ&nbsp;БАНКОМ
+						</div>
+					</div>
+					<div class="deposit-info__block">
+						<h3>Как оформить корпоративную
+							карту РОСГОССТРАХ БАНКА?</h3>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method salary-project__open-card">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _w100">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Обратиться в отделения Банка по месту обслуживания и получить необходимую консультацию
+										</div>
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Сотрудник Банка подскажет, какой пакет документов нужно сформировать, и предложит условия, наиболее подходящие именно Вашей организации
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="files _indent">
+							<a href="#" class="file w100">
+								<div class="file__inner">
+									<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
+									<div class="file__props">DOC, 296KB</div>
+								</div>
+							</a>
+							<a href="#" class="file w100">
+								<div class="file__inner">
+									<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
+									<div class="file__props">DOC, 296KB</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block _gray animation__top  _gray salary-project__benefits no-mob-block" data-tabs data-animation>
+				<div class="grid__inner">
+					<h3>
+						Преимущества корпоративных карт РОСГОССТРАХ БАНКА<br/>
+						<a href="javascript:void(0);" class="_active" data-tabs-tab="1">для Предприятия</a> /
+						<a href="javascript:void(0);" data-tabs-tab="2">для сотрудников Предприятия</a>
+					</h3>
+					<div class="corporate__credits">
+						<div class="corporate__credits-block">
+							<ul class="b-list" data-tabs-content="1">
+								<li>Значительное снижение расходов предприятия за счет экономии средств на кассовых и валютно-обменных операциях;</li>
+								<li>Упрощение процедуры осуществления выплат денежных средств сотрудникам предприятий на командировочные и представительские расходы и контроля предприятием подотчетных сумм;</li>
+								<li>Наличие у Банка собственной разветвленной сети банкоматов и более 200 точек обслуживания в различных регионах России</li>
+							</ul>
+							<ul class="b-list hide" data-tabs-content="2">
+								<li>исключение необходимости при поездках перевозить с собой крупные суммы наличных денежных средств и получения аванса на командировочные расходы;</li>
+								<li>возможность получения денежные средства со счета корпоративной карты в любой валюте в удобное время в банкоматах или пунктах выдачи наличных в любой стране мира;</li>
+								<li>возможность забронировать номер в гостинице, билет на самолет и получить другие виды услуг по бронированию с использованием корпоративной карты;</li>
+								<li>
+									возможность без взимания дополнительных комиссий оплатить с использованием корпоративной карты товары или услуги в магазинах
+									или предприятиях сервиса, по каталогам, почте или в сети Интернет;
+								</li>
+								<li>отсутствие необходимости декларирования денежных средств, размещенных на счете корпоративной карты при поездках за рубеж.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="b-block _gray  salary-project__benefits is-mob-block" data-animation>
+				<div class="grid__inner">
+					<h3>
+						Преимущества корпоративных карт РОСГОССТРАХ БАНКА<br/>для Предприятия
+					</h3>
+					<div class="corporate__credits">
+						<div class="corporate__credits-block">
+							<ul class="b-list">
+								<li>Значительное снижение расходов предприятия за счет экономии средств на кассовых и валютно-обменных операциях;</li>
+								<li>Упрощение процедуры осуществления выплат денежных средств сотрудникам предприятий на командировочные и представительские расходы и контроля предприятием подотчетных сумм;</li>
+								<li>Наличие у Банка собственной разветвленной сети банкоматов и более 200 точек обслуживания в различных регионах России</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="b-block _gray  salary-project__benefits is-mob-block">
+				<div class="grid__inner">
+					<h3>
+						Преимущества корпоративных карт РОСГОССТРАХ БАНКА<br/>для сотрудников Предприятия
+					</h3>
+					<div class="corporate__credits">
+						<div class="corporate__credits-block">
+							<ul class="b-list">
+								<li>исключение необходимости при поездках перевозить с собой крупные суммы наличных денежных средств и получения аванса на командировочные расходы;</li>
+								<li>возможность получения денежные средства со счета корпоративной карты в любой валюте в удобное время в банкоматах или пунктах выдачи наличных в любой стране мира;</li>
+								<li>возможность забронировать номер в гостинице, билет на самолет и получить другие виды услуг по бронированию с использованием корпоративной карты;</li>
+								<li>
+									возможность без взимания дополнительных комиссий оплатить с использованием корпоративной карты товары или услуги в магазинах
+									или предприятиях сервиса, по каталогам, почте или в сети Интернет;
+								</li>
+								<li>отсутствие необходимости декларирования денежных средств, размещенных на счете корпоративной карты при поездках за рубеж.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Тарифы</h2>
+					<div class="files _two-cols">
+						<?for ($i=1;$i<=2;$i++){?>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Программа ипотечного кредитования физических лиц «Своя квартира+»</div>
+								<div class="file__props">DOC, 296KB</div>
+							</div>
+						</a>
+						<?}?>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/news-gall.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
