1c571561f2fa8603bbaeb7fc0930b409adfc70b3
viktor <viktrgulakv@rambler.ru>
2017-03-24 15:49:50 +0300

    add:бизнесу главная

---
+++ static/corporate/index.php
@@ -0,0 +1,74 @@
+<?include "../includes/header.php"?>
+	<div class="home">
+		<?include "../includes/top-banner-corporate.php"?>
+		<?include "../includes/exchange.php"?>
+		<div class="home__gray b-blocks">
+			<div class="b-block animation__top corporate-nav" data-animation>
+				<div class="grid__inner">
+					<div class="corporate-nav__items">
+						<a href="#" class="corporate-nav__item _w2 _img" style="background-image: url('/static/theme/images/corporate/main/deposits.jpg');">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__item-title">Депозиты</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">8,4%</div>
+									<div class="corporate-nav__item-bottom-text">максимальная процентная ставка</div>
+								</div>
+							</div>
+						</a>
+						<a href="#" class="corporate-nav__item _base">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__item-title">Рассчетно-кассовое обслуживание</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">от 2 500 <span class="rub"></span></div>
+									<div class="corporate-nav__item-bottom-text">в месяц, без системы ДБО</div>
+								</div>
+							</div>
+						</a>
+						<a href="#" class="corporate-nav__item _base">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__item-title">Кредитование юридических лиц</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">12 месяцев</div>
+									<div class="corporate-nav__item-bottom-text">максимальный срок кредитования</div>
+								</div>
+							</div>
+						</a>
+						<a href="#" class="corporate-nav__item _base">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__item-title">Сейфовые ячейки</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">8,4%</div>
+									<div class="corporate-nav__item-bottom-text">максимальная процентная ставка</div>
+								</div>
+							</div>
+						</a>
+						<a href="#" class="corporate-nav__item _base">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__item-title">ДБО (интернет-банк)</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">1 000 <span class="rub"></span></div>
+									<div class="corporate-nav__item-bottom-text">в месяц, за РКО с ДБО</div>
+								</div>
+							</div>
+						</a>
+						<a href="#" class="corporate-nav__item _long  _base corporate-nav__salary">
+							<div class="corporate-nav__item-in">
+								<div class="corporate-nav__salary-img"></div>
+								<div class="corporate-nav__item-title">
+									Зарплатный проект<br/>
+									от РОСГОССТРАХ БАНКА
+								</div>
+								<div class="corporate-nav__item-bottom">
+									<div class="corporate-nav__item-bottom-title">более 130 000</div>
+									<div class="corporate-nav__item-bottom-text">зарплатных клиентов — убедительный аргумент!</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
---
+++ static/includes/top-banner-corporate.php
@@ -0,0 +1,51 @@
+<div class="swiper-container top-banner" data-banner-gall>
+	<div class="swiper-wrapper">
+		<div class="swiper-slide top-banner__slide">
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/corporate/main/banner.jpg');"></div>
+			<div class="top-banner__slide-block grid__inner">
+				<div class="top-banner__slide-block-in">
+					<div class="top-banner__slide-title">
+						Резервирование<br />
+						рассчетного счета
+					</div>
+					<div class="top-banner__slide-text">Эта услуга позволит вам совершать операции с номером счета <strong>практически мгновенно</strong></div>
+					<a href="/static/inner.php" class="btn _red top-banner__slide-btn">Узнать больше</a>
+				</div>
+			</div>
+		</div>
+		<div class="swiper-slide top-banner__slide">
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/rgs_main_banner2.png');"></div>
+			<div class="top-banner__slide-block grid__inner">
+				<div class="top-banner__slide-block-in">
+					<div class="top-banner__slide-title">
+						Вклад «Госстраховский»
+					</div>
+					<div class="top-banner__slide-text">Классический вклад для получения высокого дохода при фиксированных сумме и сроке вклада</div>
+					<a href="/static/deposit/gosstrakh.php" class="btn _red top-banner__slide-btn">Узнать больше</a>
+				</div>
+			</div>
+		</div>
+		<div class="swiper-slide top-banner__slide">
+			<div class="top-banner__slide-bg" style="background-image: url('/static/theme/images/home/banner/rgs_main_banner3.png');"></div>
+			<div class="top-banner__slide-block grid__inner">
+				<div class="top-banner__slide-block-in">
+					<div class="b-card _blue"></div>
+					<div class="top-banner__slide-title">
+						Отличная<br />карта
+					</div>
+					<div class="top-banner__slide-text">Удобно хранить, платить и зарабатывать</div>
+					<a href="#" class="btn _red top-banner__slide-btn">Узнать больше</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="top-banner__nav">
+		<div class="grid__inner">
+			<div class="swiper-pagination top-banner__pag" data-banner-gall-pag></div>
+			<div class="nav top-banner__btns">
+				<div class="nav__btn _prev" data-banner-gall-prev></div>
+				<div class="nav__btn _next" data-banner-gall-next></div>
+			</div>
+		</div>
+	</div>
+</div>
