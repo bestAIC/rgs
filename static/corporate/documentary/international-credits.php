297fd62fad9881652624acd46d57c9b16cb48971
viktor <viktrgulakv@rambler.ru>
2017-04-13 17:59:30 +0300

    add:Международные аккредитивы

---
+++ static/corporate/documentary/international-credits.php
@@ -0,0 +1,179 @@
+<?include "../../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/documentary.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Международные аккредитивы</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Документарные операции</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Международные аккредитивы</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info  animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block deposit-info__block">
+					<div class="base-info deposit-info__block">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Департамент корпоративного бизнеса</div>
+								<div class="base-info__block-text">
+									<a class="phone _dark" href="tel:+74959258080">(495) 925 80 80</a> &nbsp;&nbsp;доб. 077-1609
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__block is-desktop-block">
+						<h4><strong>Как выпустить покрытый международный аккредитив?</strong></h4>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _two-cols">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Откройте <a href="#">расчетный счет в РОСГОССТРАХ БАНКЕ</a>
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Предоставьте заявление на аккредитив и копию контракта/договора
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Подтвердите покрытие по аккредитиву
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">04</div>
+											Заключите соглашение с Банком об открытии аккредитива
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__block is-desktop-block">
+						<h4><strong>Как выпустить непокрытый международный аккредитив?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры, аналогичные
+							процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-subtitle">
+						Откройте международный аккредитив в РОСГОССТРАХ БАНКЕ для проведения
+						расчетов по внешнеторговым контрактам и минимизируйте коммерческие риски при проведении сделок
+					</div>
+					<div class="deposit-info__block">
+						<div class="inner__info-benefits-title">
+							<strong>Преимущества международных аккредитивов: </strong>
+						</div>
+						<ul class="b-list">
+							<li>Один из основных расчетных инструментов в системе международных расчетов</li>
+							<li>Возможность работы с мало знакомым контрагентом</li>
+							<li>Минимизация для сторон по внешнеторговому контракту рисков неплатежа за поставленный товар или платежа без последующей поставки товара</li>
+							<li>Контроль движения денежных средств по аккредитиву со стороны Банка</li>
+							<li>Универсальная нормативная база</li>
+						</ul>
+					</div>
+					<div class="deposit-info__block">
+						<div class="inner__info-benefits-title">
+							Услуги при проведении расчетов в форме аккредитивов
+						</div>
+						<ul class="b-list">
+							<li>Консультирование клиентов по вопросам документарных операций</li>
+							<li>Оказание помощи в составлении внешнеторговых контрактов с использованием документарных аккредитивов</li>
+							<li>
+								Выпуск подтверждённых и неподтвержденных аккредитивов, организация при необходимости подтверждения
+								выпускаемых аккредитивов в иностранных банках за счет имеющихся линий на РОСГОССТРАХ БАНКА
+							</li>
+							<li>Авизование и подтверждение экспортных аккредитивов</li>
+							<li>Проверка документов и платежи по аккредитивам </li>
+							<li>Содействие экспортерам в оформлении документов </li>
+							<li>Перевод (траснферация) аккредитивов </li>
+							<li>Выдача рамбурсных обязательств, платежи по рамбурсным инструкциям (для клиентов — кредитных организаций) </li>
+							<li>
+								Различные исполнения обязательств по международным аккрнедитивам:
+								<ul class="dash-list">
+									<li>по предъявлению — оплата по факту отгрузки</li>
+									<li>с отсрочкой платежа — оплата с отсрочкой после отгрузки</li>
+								</ul>							
+							</li>
+						</ul>
+					</div>
+					<div class="deposit-info__block no-desktop-block">
+						<h4><strong>Как выпустить покрытый международный аккредитив?</strong></h4>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _two-cols">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Откройте <a href="#">расчетный счет в РОСГОССТРАХ БАНКЕ</a>
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Предоставьте заявление на аккредитив и копию контракта/договора
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Подтвердите покрытие по аккредитиву
+										</div><!--
+												--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">04</div>
+											Заключите соглашение с Банком об открытии аккредитива
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-info__block no-desktop-block">
+						<h4><strong>Как выпустить непокрытый международный аккредитив?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры, аналогичные
+							процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block">
+				<div class="grid__inner">
+					<h2>Также вас может заинтересовать:</h2>
+					<div class="related-pages">
+						<a href="#" class="related-pages__item">
+							Внутрироссийские аккредитивы
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Банковские гарантии
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Международные
+							гарантии
+							<div class="related-pages__item-ico"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?include "../../includes/footer.php"?>
+
