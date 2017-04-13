add61c0d8e9304643a87f48d929b95a0baf6c6e9
viktor <viktrgulakv@rambler.ru>
2017-04-13 18:24:25 +0300

    add:Предэкспортное финансирование

---
+++ static/corporate/documentary/pre-export.php
@@ -0,0 +1,93 @@
+<?include "../../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/documentary.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Предэкспортное финансирование</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Документарные операции</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Предэкспортное финансирование</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="base-info">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Департамент корпоративного бизнеса</div>
+								<div class="base-info__block-text">
+									<a class="phone _dark" href="tel:+74959258080">(495) 925 80 80</a> &nbsp;&nbsp;доб. 077-1609
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="is-desktop-block">
+						<h4><strong>Как воспользоваться услугами предэкспортного финансирования?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры,
+							аналогичные процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-subtitle" >
+						Воспользуйтесь услугами РОСГОССТРАХ БАНКА по привлечению денежных ресурсов зарубежных банков для финансирования  экспортных сделок 
+					</div>
+					<div class="inner__info-benefits-title">
+						<strong>Преимущества предэкспортного финансирования: </strong>
+					</div>
+					<ul class="b-list deposit-info__block">
+						<li>Более низкая стоимость по сравнению с кредитными ставками вследствие снижения риска неполучения средств за экспортируемую продукцию</li>
+						<li>Получение средств для осуществления экспортной операции без отвлечения собственных оборотных средств </li>
+					</ul>
+					<div class="no-desktop-block deposit-info__block">
+						<h4><strong>Как воспользоваться услугами предэкспортного финансирования?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры,
+							аналогичные процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block">
+				<div class="grid__inner">
+					<h2>Также вас может заинтересовать:</h2>
+					<div class="related-pages">
+						<a href="#" class="related-pages__item">
+							Внутрироссийские аккредитивы
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Банковские гарантии
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Международные гарантии
+							<div class="related-pages__item-ico"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?include "../../includes/footer.php"?>
+
