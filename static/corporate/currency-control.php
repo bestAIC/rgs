a556b45a6a8a4a7d89eca358811872541bc47fee
viktor <viktrgulakv@rambler.ru>
2017-04-14 17:32:30 +0300

    Add:валютный контроль

---
+++ static/corporate/currency-control.php
@@ -0,0 +1,159 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/rko.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Валютный контроль</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Рассчетно-кассовое обслуживание</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Валютный контроль</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="deposit-open deposit-info__block is-desktop-block">
+						<h3 class="_no-top-indent">Как воспользоваться услугами по валютному контролю?</h3>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _w100">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Обратитесь в отделение РОСГОССТРАХ БАНКА, в котором открыт расчетный счет Вашей организации
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Получите консультацию специалиста Банка
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Предоставьте и подпишите необходимые документы
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-subtitle">
+						Воспользуйтесь услугами РОСГОССРТРАХ БАНКА по валютному контролю при осуществлении валютных операций в рамках действующего  законодательства РФ
+					</div>
+					<div class="deposit-info__block">
+						<div class="inner__info-benefits-title">
+							<strong>Преимущества обслуживания в РОСГОССТРАХ БАНКЕ</strong>
+						</div>
+						<ul class="b-list">
+							<li>Большой опыт работы в обслуживании экспортно-импортных сделок</li>
+							<li>Выгодные тарифы</li>
+							<li>Удобный формат работы с документами</li>
+							<li>
+								Удобный канал взаимодействия с банком: все документы, подлежащие предоставлению в Банк в рамках требований
+								валютного контроля, могут пересылаться по каналам удаленного доступа (через Интернет-Банк)
+							</li>
+							<li>Отсутствие комиссии за покупку/продажу иностранной валюты (тарификация только услуг денежных переводов и валютного контроля)</li>
+						</ul>
+					</div>
+					<div class="deposit-info__block">
+						<div class="inner__info-benefits-title">
+							<strong>Услуги по валютному контролю</strong>
+						</div>
+						<ul class="b-list no-b-indent">
+							<li>Бесплатные консультации в области валютного законодательства РФ</li>
+							<li>Помощь в оформлении паспортов сделок, заполнении справок о валютных операциях и других документов валютного контроля</li>
+							<li>Предоставление информации по запросу клиента о текущем состоянии расчётов по паспорту сделки</li>
+							<li>Предоставление копий документов валютного контроля по запросам клиентов</li>
+						</ul>
+					</div>
+					<div class="deposit-open deposit-info__block no-desktop-block">
+						<h3 class="_no-top-indent">Как воспользоваться услугами по валютному контролю?</h3>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _w100">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Обратитесь в отделение РОСГОССТРАХ БАНКА, в котором открыт расчетный счет Вашей организации
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Получите консультацию специалиста Банка
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Предоставьте и подпишите необходимые документы
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Документы валютного контроля</h2>
+					<div class="files__block">
+						<h3 class="files__block-title">Бланки</h3>
+						<div class="files">
+							<?for ($i=1;$i<=12;$i++){?>
+								<a href="#" class="file">
+									<div class="file__inner">
+										<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+										<div class="file__props">PDF, 1.16MB</div>
+									</div>
+								</a>
+							<?}?>
+						</div>
+					</div>
+					<div class="files__block">
+						<h3 class="files__block-title">Правила заполнения документов</h3>
+						<div class="files">
+							<?for ($i=1;$i<=3;$i++){?>
+								<a href="#" class="file">
+									<div class="file__inner">
+										<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+										<div class="file__props">PDF, 1.16MB</div>
+									</div>
+								</a>
+							<?}?>
+						</div>
+					</div>
+					<div class="files__block">
+						<h3 class="files__block-title">Нормативные документы по валютному законодательству РФ</h3>
+						<div class="files">
+							<?for ($i=1;$i<=5;$i++){?>
+								<a href="#" class="file">
+									<div class="file__inner">
+										<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+										<div class="file__props">PDF, 1.16MB</div>
+									</div>
+								</a>
+							<?}?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/subscription.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
