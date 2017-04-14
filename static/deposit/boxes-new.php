2334649766c12f1915c4b779aee9048f1dcafe44
viktor <viktrgulakv@rambler.ru>
2017-04-14 11:33:51 +0300

    add:новые сейфовые ячейки

---
+++ static/deposit/boxes-new.php
@@ -0,0 +1,286 @@
+<?include "../includes/header.php"?>
+	<div class="inner deposit ">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/deposit/Deposit_Boxes.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Сейфовые ячейки</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Частным клиентам</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Вложить и сохранить</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Сейфовые ячейки</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info _small-indent animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="deposit__info-banner deposit-info__block">
+						<div class="deposit__info-banner-in">
+							<div class="deposit__info-banner-title">Минимальный срок аренды</div>
+							<div class="deposit__info-banner-text">1 день</div>
+						</div>
+						<div class="deposit__info-banner-img"  style="background-image: url(<?=$desktopAndTableLand ? '/static/theme/images/info-banners/deposit/deposit_boxes_pic.png'
+							: ($tablePort ? '/static/theme/images/info-banners/deposit/deposit_boxes_tablet_pic.png':'/static/theme/images/info-banners/deposit/deposit_boxes_pic_mobile.png') ?>)"></div>
+					</div>
+					<div class="deposit-open deposit-info__block is-desktop-block">
+						<h3>Как арендовать ячейку?</h3>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _w100">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Уточните в ближайшем отделении Банка или по телефону Контакт-Центра наличие свободной
+											депозитной ячейки нужного размера и необходимый комплект документов для ее открытияа
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Обратитесь в ближайшее отделение Банка, оборудованное хранилищем индивидуальных банковских сейфов
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Предоставьте комплект документов и заключите договор аренды индивидуального банковского сейфа на необходимый срок
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">04</div>
+											Внесите арендную плату и получите ключ от индивидуального банковского сейфа
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-title deposit-info__block">
+						Доверьте РОСГОССТРАХ БАНКУ хранение антиквариата, ценных бумаг денежных средств, драгоценных металлов и документов.
+						Сведите к минимуму риски при заключении сделок и операций, связанных с наличными расчетами
+					</div>
+					<div class="inner__info-benefits-title deposit-info__block">
+						<strong>Преимущества открытия сейфовой ячейки в РОСГОССТРАХ БАНКЕ</strong>
+					</div>
+					<ul class="b-list">
+						<li>Полная конфиденциальность вложений — только сам клиент располагает информацией о содержимом арендованной ячейки</li>
+						<li>
+							Безопасность Ваших ценностей: современные средства охранной и противопожарной сигнализации,
+							системы видеонаблюдения, электронная система контроля доступа, пропускная система и круглосуточная охрана.
+						</li>
+						<li>Возможность выбрать срок аренды: от 1 дня до 12 месяцев (с возможностью продления)</li>
+						<li>Возможность выбора банковской ячейки нужного размера</li>
+						<li>Возможность оформления доверенности на Вашего представителя</li>
+						<li>Возможность возврата суммы арендной платы за неиспользованные дни аренды</li>
+						<li>Возможность пересчета денежной наличности в присутствии клиента по действующим тарифам</li>
+					</ul>
+					<div class="inner__info-benefits-btn">
+						<a href="#" class="btn _red">Арендовать ячейку</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks">
+			<div class="animation__top b-block" data-animation>
+				<div class="grid__inner">
+					<div class="deposit-info__block boxes-rent" >
+						<h3>Условия аренды банковских ячеек</h3>
+						<div class="boxes-rent__sizes">
+							<div class="boxes-rent__size _small">
+								<div class="deposit-table__title">Шириной меньше 500 мм</div>
+								<div class="deposit-table__wrap">
+									<table class="deposit-table">
+										<tr>
+											<th>Высота ячейки (мм)</th>
+											<th>до 60</th>
+											<th>61-100</th>
+											<th>101-150</th>
+											<th>151-200</th>
+											<th>201-300</th>
+											<th>301-600</th>
+											<th>601 и выше</th>
+										</tr>
+										<tr class="deposit-table__titles">
+											<td class="center">СРОК АРЕНДЫ (ДНЕЙ)</td>
+											<td class="center" colspan="7">АРЕНДНАЯ ПЛАТА ЗА КАЖДЫЙ ДЕНЬ, <span class="rub _medium"></span></td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center">1-30</td>
+											<td class="bold center">30</td>
+											<td class="bold center">15</td>
+											<td class="bold center">13</td>
+											<td class="bold center">11</td>
+											<td class="bold center">30</td>
+											<td class="bold center">15</td>
+											<td class="bold center">13</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center">31-90</td>
+											<td class="bold center">40</td>
+											<td class="bold center">18</td>
+											<td class="bold center">16</td>
+											<td class="bold center">14</td>
+											<td class="bold center">40</td>
+											<td class="bold center">18</td>
+											<td class="bold center">16</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center">91-180</td>
+											<td class="bold center">50</td>
+											<td class="bold center">22</td>
+											<td class="bold center">19</td>
+											<td class="bold center">17</td>
+											<td class="bold center">50</td>
+											<td class="bold center">22</td>
+											<td class="bold center">19</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center">181-365</td>
+											<td class="bold center">50</td>
+											<td class="bold center">22</td>
+											<td class="bold center">19</td>
+											<td class="bold center">17</td>
+											<td class="bold center">50</td>
+											<td class="bold center">22</td>
+											<td class="bold center">19</td>
+										</tr>
+									</table>
+								</div>
+							</div>
+							<div class="boxes-rent__size _big">
+								<div class="deposit-table__title">Шириной больше 500 мм</div>
+								<div class="deposit-table__wrap">
+									<table class="deposit-table">
+										<tr>
+											<th class="no-desktop-block">Высота ячейки (мм)</th>
+											<th>170-200</th>
+											<th>201-400</th>
+											<th>401 и выше</th>
+										</tr>
+										<tr class="deposit-table__titles">
+											<td class="no-desktop-block center">СРОК АРЕНДЫ (ДНЕЙ)</td>
+											<td class="center" colspan="3">АРЕНДНАЯ ПЛАТА ЗА КАЖДЫЙ ДЕНЬ, <span class="rub _medium"></span></td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center no-desktop-block">1-30</td>
+											<td class="bold center">11</td>
+											<td class="bold center">13</td>
+											<td class="bold center">11</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center no-desktop-block">31-90</td>
+											<td class="bold center">14</td>
+											<td class="bold center">16</td>
+											<td class="bold center">14</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center no-desktop-block">91-180</td>
+											<td class="bold center">17</td>
+											<td class="bold center">19</td>
+											<td class="bold center">17</td>
+										</tr>
+										<tr class="deposit-table__row">
+											<td class="bold center no-desktop-block">181-365</td>
+											<td class="bold center">17</td>
+											<td class="bold center">19</td>
+											<td class="bold center">17</td>
+										</tr>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="deposit-open deposit-info__block no-desktop-block">
+						<h3>Как арендовать ячейку?</h3>
+						<div class="deposit-open__methods">
+							<div class="deposit-open__method">
+								<div class="deposit-open__method-content">
+									<div class="deposit-open__steps _w100">
+										<div class="deposit-open__step">
+											<div class="deposit-open__step-num">01</div>
+											Уточните в ближайшем отделении Банка или по телефону Контакт-Центра наличие свободной
+											депозитной ячейки нужного размера и необходимый комплект документов для ее открытияа
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">02</div>
+											Обратитесь в ближайшее отделение Банка, оборудованное хранилищем индивидуальных банковских сейфов
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">03</div>
+											Предоставьте комплект документов и заключите договор аренды индивидуального банковского сейфа на необходимый срок
+										</div><!--
+											--><div class="deposit-open__step">
+											<div class="deposit-open__step-num">04</div>
+											Внесите арендную плату и получите ключ от индивидуального банковского сейфа
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix deposit-info__block _blocks">
+						<div class="deposit-info__left">
+							<div class="deposit-info__block inner__info-benefits">
+								<h3>Доступ к сейфовой ячейке</h3>
+								<ul class="b-list no-b-indent">
+									<li>В соответствии с режимом работы офисов Банка.</li>
+									<li>При предъявлении ключа и документа, удостоверяющего личность.</li>
+									<li>Открытие ячейки с помощью двух ключей, один из которых находится у Вас, а другой хранится в офисе Банка.</li>
+									<li>
+										Возможность предоставить право пользования сейфом третьему лицу по нотариально
+										заверенной доверенности или по соглашению к договору аренды сейфовой ячейки
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="deposit-info__right">
+							<div class="deposit-info__block">
+								<div class="frame-text _no-indent">
+									<strong>Запрещенные для хранения вещи:</strong> продукты питания и скоропортящиеся предметы; холодное и огнестрельное оружие;
+									взрывчатые, наркотические, радиоактивные, химические, бактериологические вещества, а также предметы и вещества,
+									способные нанести материальный или иной вред Банку и/или третьим лицам, здоровью людей
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block animation__top" data-animation>
+				<div class="grid__inner">
+					<h2>Документы</h2>
+					<div class="files _two-cols">
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Сборник Тарифов по выпуску и обслуживанию банковских карт с 01.08.2016</div>
+								<div class="file__props">PDF, 1.16MB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Правила выпуска и обслуживания банковских карт</div>
+								<div class="file__props">PDF, 768.81KB</div>
+							</div>
+						</a>
+						<a href="#" class="file">
+							<div class="file__inner">
+								<div class="file__name">Анкета заявление на получение банковской карты для тарифа «Карта Пенсионная»</div>
+								<div class="file__props">DOC, 89KB</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<?include "../includes/offices.php"?>
+		</div>
+	</div>
+<?include "../includes/footer.php"?>
+
