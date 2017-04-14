6ce2181eca84180d035dba89ae0f9974513b740d
viktor <viktrgulakv@rambler.ru>
2017-04-14 10:34:51 +0300

    add:Международные гарантии

---
+++ static/corporate/documentary/international-guarantees.php
@@ -0,0 +1,133 @@
+<?include "../../includes/header.php"?>
+	<div class="inner deposit">
+		<div class="inner__banner">
+			<div class="inner__banner-img">
+				<div class="inner__banner-img-in" style="background-image: url('/static/theme/images/banners/corporate/documentary.jpg');"></div>
+			</div>
+			<div class="inner__banner-in grid__inner">
+				<div class="inner__banner-block">
+					<div class="inner__banner-title">Международные гарантии</div>
+					<div class="breadcrumbs">
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Главная</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Бизнесу</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<a href="#" class="breadcrumbs__link">Документарные операции</a>
+						</div>
+						<div class="breadcrumbs__item">
+							<span>Международные гарантии</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="inner__info animation__top"  data-animation>
+			<div class="grid__inner">
+				<div class="inner__info-block">
+					<div class="base-info">
+						<div class="base-info__title">Контакты</div>
+						<div class="base-info__text">
+							<div class="base-info__block">
+								<div class="base-info__block-title">Департамент корпоративного бизнеса</div>
+								<div class="base-info__block-text">
+									<a class="phone _dark" href="tel:+74959258080">(495) 925 80 80</a> &nbsp;&nbsp;доб. 077-1609
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="is-desktop-block">
+						<h4><strong>Как оформить международную гарантию?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры,
+							аналогичные процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+				<div class="inner__info-benefits">
+					<div class="inner__info-benefits-subtitle" >
+						Воспользуйтесь одним из наиболее надежных инструментов обеспечения международных сделок – международной гарантией РОСГОССТРАХ БАНКА
+					</div>
+					<div class="inner__info-benefits-title">
+						<strong>Преимущества международных гарантий: </strong>
+					</div>
+					<ul class="b-list deposit-info__block">
+						<li>Обеспечение исполнения обязательств принципала перед бенефициаром</li>
+						<li>Возможность не отвлекать собственные средства принципала в качестве обеспечения его обязательств</li>
+						<li>Возможность получения товарного кредита от контрагента</li>
+					</ul>
+					<div class="deposit-info__block">
+						<h4><strong>Виды международных гарантий</strong></h4>
+						<ul class="b-list">
+							<li>
+								<div class="b-list__item-title">Гарантия платежа (Payment guarantee)</div>
+								выставляется банком-гарантом в обеспечение выполнения платежных обязательств
+								приказодателя/принципала (покупателя) перед бенефициаром (продавцом)
+							</li>
+							<li>
+								<div class="b-list__item-title">Гарантия возврата авансового платежа (Advance payment guarantee)</div>
+								выставляется банком-гарантом в обеспечение возврата полученного приказодателем/принципалом (продавцом)
+								авансового платежа перед бенефициаром (покупателем или заказчиком)
+							</li>
+							<li>
+								<div class="b-list__item-title">Конкурсная или тендерная гарантия (Bid bond)</div>
+								выставляется банком-гарантом в обеспечение возможности участия приказодателя/принципала (участника конкурса или тендера)
+								в конкурсе или тендере, проводимом бенефициаром (организатор конкурса или тендера)
+							</li>
+							<li>
+								<div class="b-list__item-title">Гарантия исполнения контракта (Performance bond)</div>
+								выставляется банком-гарантом в обеспечение надлежащего исполнения обязательств приказодателя/принципала (продавца)
+								по поставке товаров или выполнению работ/услуг перед бенефициаром (покупателем или заказчиком)
+							</li>
+							<li>
+								<div class="b-list__item-title">Резервные аккредитивы (Standby Letters of Credit) </div>
+							</li>
+						</ul>
+					</div>
+					<div class="deposit-info__block">
+						<h4>Гарантийные услуги</h4>
+						<ul class="b-list no-b-indent">
+							<li>Выпуск прямых гарантий РОСГОССТРАХ БАНКА и гарантий зарубежных банков против контргарантий РОСГОССТРАХ БАНКА</li>
+							<li>Авизование и сопровождение различных видов гарантий, выставленных в пользу клиентов РОСГОССТРАХ БАНКА</li>
+							<li>
+								Консультирование клиентов по вопросам работы с банковскими гарантиями и соблюдения норм Международных унифицированных правил
+								для гарантий по требованию и требований Международной практики для резервных аккредитивов
+							</li>
+						</ul>
+					</div>
+					<div class="deposit-info__block no-desktop-block">
+						<h4><strong>Как оформить международную гарантию?</strong></h4>
+						<div class="frame-text _no-indent">
+							При выпуске непокрытого аккредитива применяются процедуры,
+							аналогичные процедурам выдачи кредита в части установления кредитных рисков на клиента Банка
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="b-blocks _gray">
+			<div class="b-block">
+				<div class="grid__inner">
+					<h2>Также вас может заинтересовать:</h2>
+					<div class="related-pages">
+						<a href="#" class="related-pages__item">
+							Внутрироссийские аккредитивы
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Банковские гарантии
+							<div class="related-pages__item-ico"></div>
+						</a>
+						<a href="#" class="related-pages__item">
+							Предэкспортное финансирование
+							<div class="related-pages__item-ico"></div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?include "../../includes/footer.php"?>
+
