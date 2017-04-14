da0fb4545166eeafb82b5963d130a4dbb08cfaad
viktor <viktrgulakv@rambler.ru>
2017-04-14 13:35:35 +0300

    upd:Размещение свободных средств

--- static/corporate/investment/index.php
+++ static/corporate/investment/index.php
@@ -26,11 +26,6 @@
 				<div class="inner__info-block">
 					<div class="base-info">
 						<div class="base-info__title">Контакты</div>
-						<div class="base-info__subtitle">
-							Сотрудники РОСГОССТРАХ БАНКА помогут Вам в оформлении необходимых документов
-							и окажут квалифицированную информационную поддержку.
-							Более подробную информацию Вы можете получить по телефонам:
-						</div>
 						<div class="base-info__text">
 							<div class="base-info__block">
 								<div class="base-info__block-title">для Москвы:</div>
@@ -49,82 +44,50 @@
 				</div>
 				<div class="inner__info-benefits">
 					<div class="inner__info-benefits-title">
-						Размещение временно свободных денежных средств позволяет извлечь дополнительную прибыль из собственной операционной
-						деятельности и заставить деньги работать без пауз.<br/>
-						Мы поможем Вам в этом.
-					</div>
-					<div class="inner__info-benefits-text _big">
-						РОСГОССТРАХ БАНК предлагает услуги по размещению временно свободных денежных средств юридических лиц в рублях
-						и иностранной валюте в депозиты и векселя Банка на различные сроки, а также начисляет проценты на остатки по расчетным счетам.
+						Разместите временно свободные денежные средства в РОСГОССТРАХ БАНКЕ
+						и извлеките дополнительную прибыль из собственной операционной деятельности
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="b-blocks _gray">
-			<div class="b-block animation__top corporate-invest-nav page-nav" data-animation>
-				<div class="grid__inner">
-					<div class="page-nav__row">
-						<div class="page-nav__row-col">
-							<a href="#" class="page-nav__item _big corporate-invest-nav__deposits">
-								<div class="corporate-invest-nav__deposits-img"></div>
-								<div class="page-nav__item-bottom">
-									<div class="page-nav__item-title">
-										Депозиты
-									</div>
-									<div class="page-nav__item-text">
-										Размещение временно свободных денежных средств на выгодных условиях
-									</div>
-								</div>
-							</a>
-						</div>
-						<div class="page-nav__row-col _items">
-							<a href="#" class="page-nav__item">
+		<div class="b-block _gray animation__top corporate-invest-nav page-nav" data-animation>
+			<div class="grid__inner">
+				<div class="page-nav__row">
+					<div class="page-nav__row-col">
+						<a href="#" class="page-nav__item _big corporate-invest-nav__deposits">
+							<div class="corporate-invest-nav__deposits-img"></div>
+							<div class="page-nav__item-bottom">
 								<div class="page-nav__item-title">
-									Собственные векселя
+									Депозиты
 								</div>
 								<div class="page-nav__item-text">
-									Широкие возможности приобретения и погашения собственных векселей Банка
+									Размещение временно свободных денежных средств на выгодных условиях
 								</div>
-							</a>
-							<a href="#" class="page-nav__item">
-								<div class="page-nav__item-title">
-									Остатки по расчетным счетам
-								</div>
-								<div class="page-nav__item-text">
-									Можете заключить договор о начислении процентов на остатки по расчетным счетам
-								</div>
-							</a>
-						</div>
+							</div>
+						</a>
+					</div>
+					<div class="page-nav__row-col _items">
+						<a href="#" class="page-nav__item">
+							<div class="page-nav__item-ico _base _investment1"></div>
+							<div class="page-nav__item-title">
+								Векселя
+							</div>
+							<div class="page-nav__item-text">
+								Широкие возможности приобретения и погашения собственных векселей Банка
+							</div>
+						</a>
+						<a href="#" class="page-nav__item">
+							<div class="page-nav__item-ico _base _investment2"></div>
+							<div class="page-nav__item-title">
+								Остатки
+							</div>
+							<div class="page-nav__item-text">
+								Можете заключить договор о начислении процентов на остатки по расчетным счетам
+							</div>
+						</a>
 					</div>
 				</div>
 			</div>
-			<div class="b-block _white animation__top corporate-invest-nav__bottom" data-animation>
-				<div class="deposit-info__block">
-					<div class="grid__inner">
-						<div class="inner__info-benefits">
-							<div class="inner__info-benefits-title">
-								Размещение свободных средств
-								в РОСГОССТРАХ&nbsp;БАНКЕ — это:
-							</div>
-							<ul class="b-list no-b-indent">
-								<li>Конкурентоспособные процентные ставки</li>
-								<li>Простая процедура оформления</li>
-								<li>Гарантия сохранности средств</li>
-								<li>Возможность установления индивидуальных условий по сроку и ставке</li>
-								<li>Выплата процентов по взаимной договоренности сторон ежемесячно или в день возврата вклада</li>
-								<li>Возможность страхования и пролонгации вклада
-									по вашему желанию</li>
-							</ul>
-						</div>
-						<div class="inner__info-block">
-							<div class="frame-text _no-indent">
-								Получить подробную информацию об условиях размещения свободных денежных средств Вы можете в ближайшем офисе Банка, обслуживающего юридических лиц
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?include "../../includes/news-gall.php"?>
 		</div>
 	</div>
 <?include "../../includes/footer.php"?>
