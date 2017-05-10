dfa0ca76c6b91427fc0da48ded0e20c1251cb752
viktor <viktrgulakv@rambler.ru>
2017-05-10 16:24:06 +0300

    add:заявка на консультацию

--- static/corporate/index.php
+++ static/corporate/index.php
@@ -20,11 +20,20 @@
 						</p>
 						<p class="_top-indent"><strong>Преимущества обслуживания в РОСГОССТРАХ БАНКЕ</strong></p>
 					</div>
-					<ul class="b-list no-b-indent">
+					<ul class="b-list no-b-indent deposit-info__block">
 						<li>Индивидуальный подход к каждому клиенту и внимательное отношение ко всем Вашим пожеланиям</li>
 						<li>Гибкие условия по широкой линейке продуктов</li>
 						<li>Высокая степень надежности проводимых операций</li>
 					</ul>
+					<div class="deposit-info__block">
+						<div class="inner__info-benefits-title"><strong>Есть вопросы?</strong></div>
+						<div class="inner__info-benefits-subtitle _small-indent30">
+							Вы всегда сможете оставить заявку на консультацию и наши специалисты ответят на любой возникший вопрос.
+						</div>
+						<div class="">
+							<a href="javascript:void(0);" data-base-form-show data-popup-show="consultation" class="btn _red">Оставить заявку</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -94,6 +103,139 @@
 				</div>
 			</div>
 			<?include "../includes/news-gall__only-news.php"?>
+		</div>
+		<div class="hide">
+			<div class="b-form big-form guarantee-form" data-popup="consultation" data-base-form-wrap>
+				<form action="/static/ajax/askForm.php">
+					<h2>Заявка на консультацию</h2>
+					<div class="subtitle">
+						Воспользуйтесь данной формой, если хотите получить консультацию по банковским продуктам.
+						Если вы хотите отправить отзыв о работе Банка – воспользуйтесь
+						формой <a href="javascript:void(0);" data-ask-form-show="review">«Отправить отзыв»</a>
+					</div>
+					<div class="b-form__content">
+						<div class="b-form__content-cols">
+							<div class="b-form__content-col">
+								<div class="b-form__content-title">Информация о Вас</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<div class="b-form__block-title">ФАМИЛИЯ имя отчество <span class="required">*</span></div>
+										<div class="b-form__block-field">
+											<input type="text" placeholder="Иванов Иван Иванович">
+										</div>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<div class="b-form__block-title">ЭЛЕКТРОННАЯ ПОЧТА <span class="required">*</span></div>
+										<div class="b-form__block-field">
+											<input type="email" placeholder="ivanivanov@mail.ru">
+										</div>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<div class="b-form__block-title">Телефон для обратной связи <span class="required">*</span></div>
+										<div class="b-form__block-field phone-inp">
+											<input type="text" data-phone-mask placeholder="(495) 078-98-98 ">
+										</div>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block _small-select">
+										<div class="b-form__block-title">регион <span class="required">*</span></div>
+										<select data-chosen data-placeholder="Ваш регион">
+											<option value="">Ваш регион</option>
+											<option value="Москва">Москва</option>
+											<option value="Москва">Москва</option>
+											<option value="Москва">Москва</option>
+										</select>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block _small-select">
+										<div class="b-form__block-title">предпочтительный формат обратной связи <span class="required">*</span></div>
+										<select data-chosen data-placeholder="Выберите">
+											<option value="">Выберите</option>
+											<option value="звонок специалиста">звонок специалиста</option>
+											<option value="ответ по почте">ответ по почте</option>
+										</select>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<div class="b-form__block-title">предпочтительные дата и время звонка <span class="required">*</span></div>
+										<div class="b-form__block-field">
+											<input type="text" placeholder="например, 28 июня, в 9 утра">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="b-form__content-col">
+								<div class="b-form__content-title">Тема консультации</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<div class="b-form__block-title">интересующие компанию банковские продукты <span class="required">*</span></div>
+										<div class="b-form__checks _indent">
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Кредитование</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Банковские карты</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Расчетно-кассовое обслуживание</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Размещение свободных средств</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Дистанционно-банковское обслуживание</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Банковские гарантии</label>
+											</div>
+											<div class="b-form__check" data-base-check-wrap>
+												<label><input data-base-check type="checkbox">Международное финансирование, документарные операции</label>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block-title">Текст сообщения</div>
+									<div class="b-form__block _textarea _small">
+										<textarea placeholder="Дополнительные комментарии"></textarea>
+									</div>
+								</div>
+								<div class="b-form__row">
+									<div class="b-form__block">
+										<label class="reserve-form__check"><input type="checkbox" >Я согласен с <a href="#" target="_blank">условиями обработки данных</a></label>
+									</div>
+								</div>
+								<div class="b-form__captcha b-captcha">
+									<div class="g-recaptcha" data-sitekey="test2"></div>
+								</div>
+								<div class="b-form__errors" data-form-errors></div>
+							</div>
+						</div>
+
+					</div>
+					<div class="b-form__btn-wrap">
+						<button type="submit" class="btn _red b-form__btn guarantee-form__btn">отправить</button>
+					</div>
+				</form>
+				<div class="b-form__success hide" data-success>
+					<div class="b-form__success-in">
+						<div class="b-form__success-ico"></div>
+						<div class="b-form__success-block">
+							<div class="b-form__success-title">Спасибо!</div>
+							<div class="b-form__success-text">
+								Ваш заявка отправлена.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 <?include "../includes/footer.php"?>
